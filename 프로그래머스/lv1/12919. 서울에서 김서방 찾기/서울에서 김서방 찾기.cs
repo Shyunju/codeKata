eb8a8810a41f9509932eae0c4267245f3e3ca7df
Shyunju <71755508+Shyunju@users.noreply.github.com>
2023-08-29 09:16:05 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.41 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public string solution(string[] seoul) {
+        string answer = "";
+        int kim = 0;
+        for(int i =0; i < seoul.Length ; i++){
+            if(seoul[i] == "Kim"){
+                kim = i;
+            }
+        }
+        return answer = "김서방은 "+ kim.ToString() + "에 있다";
+    }
+}
