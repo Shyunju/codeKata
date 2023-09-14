a5451bd78cb491e7ae84f792fe8b5a4481b6e600
Shyunju <71755508+Shyunju@users.noreply.github.com>
2023-09-14 09:26:53 +0900

    [unrated] Title: 과일 장수, Time: 1.51 ms, Memory: 31.5 MB -BaekjoonHub

--- 프로그래머스/unrated/135808. 과일 장수/과일 장수.cs
+++ 프로그래머스/unrated/135808. 과일 장수/과일 장수.cs
@@ -3,13 +3,9 @@ using System;
 public class Solution {
     public int solution(int k, int m, int[] score) {
         int answer = 0;
-        int n = m -1;
         Array.Sort(score);
-        Array.Reverse(score);
-        for(int i =0 ; i < score.Length / m; i++){
-            answer += score[n] * m;
-            n += m;
-
+        for(int i =score.Length -m ; i >= 0; i-=m){
+            answer += score[i] * m;
         }
         return answer;
     }
