5a60622b0e81724ed5d20c2c157d789e59429a10
Shyunju <71755508+Shyunju@users.noreply.github.com>
2024-01-03 16:05:23 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.90 ms, Memory: 31.2 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.cs
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.cs
@@ -3,18 +3,17 @@ using System;
 public class Solution {
     public string solution(int[] food) {
         string answer = "";
-        string result = "";
         for( int i = 1; i< food.Length; i++){
             for(int j = food[i] / 2; j>=0 ; j--){
                 if( j == 0)  break;
-                result += i.ToString();
+                answer += i.ToString();
             }
         }
-        char[] reverse = result.ToCharArray();
+        char[] reverse = answer.ToCharArray();
         Array.Reverse(reverse);
         string sReverse = new string(reverse);
         
-        answer = result + "0" + sReverse;
+        answer += "0" + sReverse;
         return answer;
     }
 }
