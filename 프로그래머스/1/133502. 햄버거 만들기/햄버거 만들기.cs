44eaab4f3b59c5169651f219d24322d6a94057bc
Shyunju <71755508+Shyunju@users.noreply.github.com>
2024-01-07 18:12:26 +0900

    [level 1] Title: 햄버거 만들기, Time: 1.45 ms, Memory: 31.4 MB -BaekjoonHub

--- 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.cs
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.cs
@@ -1,24 +1,35 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 public class Solution {
     public int solution(int[] ingredient) {
         int answer = 0;
-        List<int> list = new List<int>();
+        bool success = true;
         
-        foreach(int burger in ingredient)
-        {
-            list.Add(burger);
+        int[] recipe = new int[4]{1, 3, 2, 1};
+        var stack = new Stack<int>();
+        
+        for(int i = 0; i < ingredient.Length; i++){
+            stack.Push(ingredient[i]);
             
-            if(list.Count >= 4)
-            {           
-                if(list[list.Count - 4] == 1 && list[list.Count - 3] == 2 && list[list.Count - 2] == 3 && list[list.Count - 1] == 1)
-                {
+            if(stack.Count() >= 4){
+                success = true;
+                for( int j = 0; j < 4; j++){
+                    if(stack.ElementAt(j) != recipe[j]){
+                        success = false;
+                        break;
+                    }
+                }
+                if(success){
+                    for(int k = 0; k < 4; k++){
+                        stack.Pop();
+                    }
                     answer++;
-                    list.RemoveRange(list.Count - 4, 4);
                 }
             }
         }
+
         return answer;
     }
 }
