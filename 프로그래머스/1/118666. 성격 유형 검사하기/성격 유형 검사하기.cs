a52446aa6e352310673e35bc6d46bdc14f463b64
Shyunju <71755508+Shyunju@users.noreply.github.com>
2024-01-08 17:01:24 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 3.15 ms, Memory: 31.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+
+public class Solution {
+    public string solution(string[] survey, int[] choices) {
+        string answer = "";
+        Dictionary<char, int> dic = new Dictionary<char, int>
+        {
+            {'R', 0}, {'T', 0}, {'C', 0}, {'F', 0}, 
+            {'J', 0}, {'M', 0}, {'A', 0}, {'N', 0}
+        };
+        
+        for(int i = 0; i < choices.Length; i++)
+        {
+            if(choices[i] < 4){
+                dic[survey[i][0]] += 4 - choices[i];
+            }else if(choices[i] > 4){
+                dic[survey[i][1]] += choices[i] - 4;
+            }
+        }
+        
+        answer += (dic['R'] >= dic['T']) ? "R" : "T";
+        answer += (dic['C'] >= dic['F']) ? "C" : "F";
+        answer += (dic['J'] >= dic['M']) ? "J" : "M";
+        answer += (dic['A'] >= dic['N']) ? "A" : "N";
+        return answer;
+    }
+}
