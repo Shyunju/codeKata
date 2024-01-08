680d8081801c24c06895ae5424550acca03402cd
Shyunju <71755508+Shyunju@users.noreply.github.com>
2024-01-08 16:37:56 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.87 ms, Memory: 31.5 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.cs
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.cs
@@ -1,13 +1,22 @@
 using System;
+using static System.Math;
+using System.Collections.Generic;
 
 public class Solution {
     public int solution(int n) {
         int answer=0;
-    while(n>0){
-        answer*=3;
-        answer+=n%3;
-        n/=3;
-    }
-    return answer;
+        List<int> list = new List<int>();
+        while(n>0){
+            int num = n %3;
+            list.Add(num);
+            n /= 3;
+        }
+        list.Reverse();
+        
+        for(int i =0; i< list.Count; i++){
+            answer += (int)(list[i] * Math.Pow(3, i));
+        }
+    
+        return answer;
     }
 }
