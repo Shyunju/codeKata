52eb1f76e819d53376a9ffee434b62b3de931da6
Shyunju <71755508+Shyunju@users.noreply.github.com>
2024-01-14 23:28:07 +0900

    [level Lv.2] Title: 게임 맵 최단거리, Time: 1.20 ms, Memory: 31.6 MB -BaekjoonHub

--- 프로그래머스/Lv.2/1844. 게임 맵 최단거리/게임 맵 최단거리.cs
+++ 프로그래머스/Lv.2/1844. 게임 맵 최단거리/게임 맵 최단거리.cs
@@ -1,64 +1,44 @@
 using System;
-    using System.Collections.Generic;
+using System.Collections.Generic;
 
-    class Solution
+class Solution {
+    int n, m =0;
+    
+    int[] dx = {-1, 1, 0, 0};
+    int[] dy = {0, 0, -1, 1};
+    
+    Queue<(int, int)> q = new Queue<(int, int)>();
+    public int solution(int[,] maps) {
+        n = maps.GetLength(0);
+        m = maps.GetLength(1);
+        
+        Bfs(0,0, maps);
+        
+        if(maps[n-1, m-1] == 1) return -1;
+        else return (maps[n-1, m-1]);
+    }
+    
+    public void Bfs(int i, int j, int[,] arr)
     {
-        public class Pos
+        q.Enqueue((i,j));
+        
+        while(q.Count != 0)
         {
-            public int y;
-            public int x;
-            public int count; // 이동 횟수
-
-            public Pos(int y, int x, int count)
+            (int a, int b) = q.Dequeue();
+            
+            for(int k = 0; k < 4; k++)
             {
-                this.y = y;
-                this.x = x;
-                this.count = count;
-            }
-        }
-
-        public int solution(int[,] maps)
-        {
-            // maps를 방문배열로 활용 (방문 : 0, 미방문 : 1)
-            var dirY = new int[4] { 0, 0, 1, -1 };
-            var dirX = new int[4] { 1, -1, 0, 0 };
-            var queue = new Queue<Pos>();
-
-            // 시작좌표
-            queue.Enqueue(new Pos(0, 0, 1));
-            maps[0, 0] = 0;
-           
-            // BFS
-            while (queue.Count != 0)
-            {
-                // 현재 위치
-                Pos currPos = queue.Dequeue();
+                int nx = a + dx[k];
+                int ny = b + dy[k];
                 
-                // dirX, dirY를 이용해 현재 위치에서 이동 가능한 4방향을 나타냄
-                for (int i = 0; i < 4; i++)
+                if( nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+                if(arr[nx, ny] == 0) continue;
+                if(arr[nx, ny] == 1)
                 {
-                    // 이동하려는 위치
-                    Pos movePos = new Pos(currPos.y + dirY[i], currPos.x + dirX[i], currPos.count + 1);
-
-                    // 배열 범위 범어남 (게임 맵을 벗어남)
-                    if (movePos.y < 0 || movePos.y >= maps.GetLength(0) ||
-                       movePos.x < 0 || movePos.x >= maps.GetLength(1))
-                        continue;
-
-                    // 적 진영에 도착
-                    if (movePos.y == maps.GetLength(0) - 1 &&
-                       movePos.x == maps.GetLength(1) - 1)
-                        return movePos.count;
-
-                    // 벽이거나 이미 탐색된 길
-                    if (maps[movePos.y, movePos.x] == 0)
-                        continue;
-
-                    maps[movePos.y, movePos.x] = 0; // 방문처리
-                    queue.Enqueue(movePos); // 다음 탐색 대상
+                    arr[nx, ny] = arr[a, b] + 1;
+                    q.Enqueue((nx, ny));
                 }
             }
-
-            return -1;
         }
     }
+}
