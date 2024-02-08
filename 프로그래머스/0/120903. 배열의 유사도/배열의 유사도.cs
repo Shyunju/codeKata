c7bb44c85d1c9c89be0e1897d9004ab18219b1e5
SHIN HYUNJU <71755508+Shyunju@users.noreply.github.com>
2024-02-08 19:48:42 +0900

    [level 0] Title: 배열의 유사도, Time: 0.75 ms, Memory: 31.2 MB -BaekjoonHub

--- 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.cs
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.cs
@@ -3,27 +3,7 @@ using System.Linq;
 
 public class Solution {
     public int solution(string[] s1, string[] s2) {
-        int answer = 0;
-        int idx = s1.Length >= s2.Length ? s2.Length : s1.Length;
-        if(idx == s1.Length)
-        {
-            answer = contain(s1, s2, idx);
-        }else{
-            answer = contain(s2, s1, idx);
-        }
-        return answer;
-        
-    }
-    
-    public int contain(string[] arr, string[] darr, int idx)
-    {
-        int answer = 0;
-        for(int i = 0; i < idx ; i++)
-        {
-            if(Array.Exists(darr, x => x == arr[i]))
-                answer++;
-        }
-        
+        int answer = s1.Count(x => s2.Contains(x));
         return answer;
     }
 }
