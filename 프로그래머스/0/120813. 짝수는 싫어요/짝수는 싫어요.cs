bf53f630e68f5a0063b5f0df8073d04533f93425
SHIN HYUNJU <71755508+Shyunju@users.noreply.github.com>
2024-02-14 20:54:37 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.53 ms, Memory: 31.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+
+public class Solution {
+    public int[] solution(int n) {
+        List<int> answer = new List<int>();
+        for(int i = 1 ; i <= n ; i++)
+        {
+            if(i % 2 == 1)
+                answer.Add(i);
+        }
+        return answer.ToArray();
+    }
+}
