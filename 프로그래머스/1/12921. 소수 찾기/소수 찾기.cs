2cff25b88cebba295d77448cc2e13849c770b33c
SHIN HYUNJU <71755508+Shyunju@users.noreply.github.com>
2024-02-15 21:22:53 +0900

    [level 1] Title: 소수 찾기, Time: 187.22 ms, Memory: 31.4 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.cs
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.cs
@@ -1,19 +1,18 @@
-using System;
 public class Solution {
-    public int solution(int n) {
-        int answer = 0;
-        bool[] sosu =new bool [n+1];
-        for(int i=2; i<=n ; i++) sosu[i]=true;
-        int root=(int)Math.Sqrt(n);
-        for(int i=2; i<=root; i++){ 
-            if(sosu[i]==true){ 
-                for(int j=i; i*j<=n; j++)
-                    sosu[i*j]=false;
-            }
-        } for(int i =2; i<=n; i++) 
-        { 
-            if(sosu[i]==true) answer++;
-         }
-        return answer;
-    }
+  bool IsPrime(int num)
+        {
+            for (int i = 2; i * i <= num ; i++)            
+                if (num % i == 0)                
+                    return false;                            
+            return true;
+        }
+        public int solution(int n)
+        {
+            int answer = 0;
+
+            for (int i = 2; i <= n; i++)            
+                if (IsPrime(i))                
+                    answer++;
+            return answer;
+        }
 }
