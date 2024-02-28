6a2cafd837ce8ab948cf7a185ae3a4a5670bdfe6
SHIN HYUNJU <71755508+Shyunju@users.noreply.github.com>
2024-02-28 18:58:54 +0900

    [level 0] Title: A로 B 만들기, Time: 4.07 ms, Memory: 31.3 MB -BaekjoonHub

--- 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.cs
+++ 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.cs
@@ -1,24 +1,8 @@
 using System;
-using System.Collections.Generic;
-
+using System.Linq;
 public class Solution {
     public int solution(string before, string after) {
-        int answer = 1;
-        List<string> b_list = new List<string>();
-        List<string> a_list = new List<string>();
-        
-        for(int i = 0; i < before.Length; i++)
-        {
-            b_list.Add(before[i].ToString());
-            a_list.Add(after[i].ToString());
-        }
-        for(int i = 0; i < before.Length; i++)
-        {
-            if(a_list.Contains(b_list[i].ToString()))
-            {
-                a_list.Remove(b_list[i].ToString());
-            }
-        }
-        return answer = a_list.Count == 0 ? 1 : 0;
+        int answer = String.Concat(before.OrderBy(x=>x)) == String.Concat(after.OrderBy(x=>x)) ? 1 : 0;
+        return answer;
     }
 }
