d2b43ae817ec92dccb36c1a93762cc739b897ac3
SHIN HYUNJU <71755508+Shyunju@users.noreply.github.com>
2024-02-29 21:48:21 +0900

    [level 0] Title: 평행, Time: 0.34 ms, Memory: 31 MB -BaekjoonHub

--- 프로그래머스/0/120875. 평행/평행.cs
+++ 프로그래머스/0/120875. 평행/평행.cs
@@ -4,9 +4,7 @@ using System.Linq;
 
 public class Solution {
     public int solution(int[,] dots) {
-        List<float> slopes = new List<float>();
-        int length = dots.GetLength(0);
-        
+
         if((dots[0,1] - dots[1,1]) / (float)(dots[0,0] - dots[1,0]) == (dots[2,1] - dots[3,1]) / (float)(dots[2,0] - dots[3,0])){
             return 1;
         }
@@ -18,8 +16,7 @@ public class Solution {
         if((dots[0,1] - dots[3,1]) / (float)(dots[3,0] - dots[1,0]) == (dots[1,1] - dots[2,1]) / (float)(dots[1,0] - dots[2,0])){
             return 1;
         }
-        //slopes.Add((dots[0,1] - dots[1,1]) / (float)(dots[0,0] - dots[1,0]));
-        //return slopes.Distinct().Count() == length * (length-1) / 2 ? 0 : 1;
+        
         return 0;
     }
 }
