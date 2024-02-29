1e79db83d5fc0e875d3a04720f7732cf013c01fe
SHIN HYUNJU <71755508+Shyunju@users.noreply.github.com>
2024-02-29 21:46:58 +0900

    [level 0] Title: 평행, Time: 0.57 ms, Memory: 31.3 MB -BaekjoonHub

--- 프로그래머스/0/120875. 평행/평행.cs
+++ 프로그래머스/0/120875. 평행/평행.cs
@@ -1,20 +1,25 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 public class Solution {
     public int solution(int[,] dots) {
-        if ((double)(dots[0, 0] - dots[1, 0]) / (dots[0, 1] - dots[1, 1]) == (double)(dots[2, 0] - dots[3, 0]) / (dots[2, 1] - dots[3, 1]))
-        {
+        List<float> slopes = new List<float>();
+        int length = dots.GetLength(0);
+        
+        if((dots[0,1] - dots[1,1]) / (float)(dots[0,0] - dots[1,0]) == (dots[2,1] - dots[3,1]) / (float)(dots[2,0] - dots[3,0])){
             return 1;
         }
-        if ((double)(dots[0, 0] - dots[2, 0]) / (dots[0, 1] - dots[2, 1]) == (double)(dots[1, 0] - dots[3, 0]) / (dots[1, 1] - dots[3, 1]))
-        {
+        
+        if((dots[0,1] - dots[2,1]) / (float)(dots[0,0] - dots[2,0]) == (dots[1,1] - dots[3,1]) / (float)(dots[1,0] - dots[3,0])){
             return 1;
         }
-        if ((double)(dots[0, 0] - dots[3, 0]) / (dots[0, 1] - dots[3, 1]) == (double)(dots[1, 0] - dots[2, 0]) / (dots[1, 1] - dots[2, 1]))
-        {
+                                             
+        if((dots[0,1] - dots[3,1]) / (float)(dots[3,0] - dots[1,0]) == (dots[1,1] - dots[2,1]) / (float)(dots[1,0] - dots[2,0])){
             return 1;
         }
-
+        //slopes.Add((dots[0,1] - dots[1,1]) / (float)(dots[0,0] - dots[1,0]));
+        //return slopes.Distinct().Count() == length * (length-1) / 2 ? 0 : 1;
         return 0;
     }
 }
