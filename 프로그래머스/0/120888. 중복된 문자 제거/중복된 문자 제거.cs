483305e79f3349acb2e2fdfd1bf161b3b375ac81
SHIN HYUNJU <71755508+Shyunju@users.noreply.github.com>
2024-03-04 19:27:04 +0900

    [level 0] Title: 중복된 문자 제거, Time: 2.24 ms, Memory: 31 MB -BaekjoonHub

--- 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.cs
+++ 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.cs
@@ -1,16 +1,10 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
 
 public class Solution {
     public string solution(string my_string) {
-        string answer = "";
-        List<string> result = new List<string>();
-        for(int i = 0; i < my_string.Length; i++)
-        {
-            result.Add(my_string[i].ToString());
-        }
-        result = result.Distinct().ToList();
-        return answer = String.Join("", result);
+        string answer = String.Concat(my_string.Distinct());
+        return answer;
+        
     }
 }
