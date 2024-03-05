1ad9d8f255c3be88f24521ed8c38d4846a2c2bec
SHIN HYUNJU <71755508+Shyunju@users.noreply.github.com>
2024-03-05 19:04:30 +0900

    [level 0] Title: 직각삼각형 출력하기, Time: 19.28 ms, Memory: 16.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120823. 직각삼각형 출력하기/직각삼각형 출력하기.cs
@@ -0,0 +1,23 @@
+using System;
+
+public class Example
+{
+    public static void Main()
+    {
+        String[] s;
+
+        Console.Clear();
+        s = Console.ReadLine().Split(' ');
+
+        int n = Int32.Parse(s[0]);
+
+        for(int i = 1; i <= n; i++)
+        {
+            for(int j = 1; j <= i; j++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine("");
+        }
+    }
+}
