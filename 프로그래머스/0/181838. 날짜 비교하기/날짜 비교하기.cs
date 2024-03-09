c708bde7275730ce6e728eda5ca02e65282369f9
SHIN HYUNJU <71755508+Shyunju@users.noreply.github.com>
2024-03-09 17:16:18 +0900

    [level 0] Title: 날짜 비교하기, Time: 0.31 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.cs
@@ -0,0 +1,25 @@
+using System;
+
+public class Solution {
+    public int solution(int[] date1, int[] date2) {
+        if(date1[0] < date2[0])
+            return 1;
+        
+        if(date1[0] == date2[0])
+        {
+            if(date1[1] > date2[1])
+                return 0;
+            else if(date1[1] == date2[1])
+            {
+                if(date1[2] < date2[2])
+                    return 1;
+                return 0;
+            }
+            return 1;
+        }
+        else
+            return 0;
+        
+        
+    }
+}
