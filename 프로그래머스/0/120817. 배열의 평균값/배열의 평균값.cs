2037a1e31c93f5b5fe148f67c313c1b8fe522c5f
SHIN HYUNJU <71755508+Shyunju@users.noreply.github.com>
2024-03-17 19:54:22 +0900

    [level 0] Title: 배열의 평균값, Time: 0.84 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.cs
@@ -0,0 +1,9 @@
+using System;
+using System.Linq;
+
+public class Solution {
+    public double solution(int[] numbers) {
+        double answer = numbers.Average();
+        return answer;
+    }
+}
