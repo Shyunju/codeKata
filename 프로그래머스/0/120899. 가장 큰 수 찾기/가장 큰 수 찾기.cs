caf96161b0cede83c7bd34d76bb1212f1517f797
SHIN HYUNJU <71755508+Shyunju@users.noreply.github.com>
2024-03-29 13:44:30 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 1.06 ms, Memory: 31.4 MB -BaekjoonHub

--- 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.cs
+++ 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.cs
@@ -1,15 +1,9 @@
 using System;
+using System.Linq;
 
 public class Solution {
     public int[] solution(int[] array) {
-        int[] answer = new int[2];
-        int max = 0;
-        foreach(int i in array){
-            if(i > max)
-                max = i;
-        }
-        answer[0] = max;
-        answer[1] = Array.IndexOf(array, max);
+        int[] answer = new int[2] {array.Max(), Array.IndexOf(array, array.Max())};
         return answer;
     }
 }
