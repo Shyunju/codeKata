b74ef5d9a1cf3049c7e626d0de170c5bb3612a53
SHIN HYUNJU <71755508+Shyunju@users.noreply.github.com>
2024-04-05 13:54:30 +0900

    [level 0] Title: 최빈값 구하기, Time: 5.33 ms, Memory: 30.9 MB -BaekjoonHub

--- 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.cs
+++ 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.cs
@@ -2,33 +2,35 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-public class Solution {
-    public int solution(int[] array) {
+public class Solution
+{
+    public int solution(int[] array)
+    {
         int answer = 0;
-        Dictionary<int, int> dict = new Dictionary<int, int>();
-        List<int> count = new List<int>();
-        for(int i = 0; i< array.Length; i++)
+
+        Dictionary<int, int> pair = new Dictionary<int, int>();
+        List<int> countMax = new List<int>();
+
+        for(int i=0; i<array.Length; i++)
         {
-            if(dict.ContainsKey(array[i]))
+            if (pair.ContainsKey(array[i]))
+                pair[array[i]]++;
+            else
+                pair.Add(array[i], 1);
+        }
+
+        int max = pair.Values.Max();
+        for(int i=0; i<array.Length; i++)
+        {
+            if (array.Count(x => x == array[i]) == max)
             {
-                dict[array[i]]++;
-            }else{
-                dict.Add(array[i], 1);
+                if (countMax.Contains(array[i]))
+                    continue;
+                else
+                    countMax.Add(array[i]);
             }
         }
-        int max = dict.Values.Max();
-        foreach(var item in dict)
-        {
-            if(item.Value == max)
-            {
-                if(answer != 0)
-                {
-                    answer = -1;
-                    break;
-                }
-                answer = item.Key;
-            }
-        }
-        return answer;
+
+        return countMax.Count > 1 ? -1 : countMax[0];
     }
 }
