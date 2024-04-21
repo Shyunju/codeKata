5d1fb4a9ec9906951f5a0dad1049fce7a249f1b7
SHIN HYUNJU <71755508+Shyunju@users.noreply.github.com>
2024-04-21 14:42:01 +0900

    [level 0] Title: 직사각형 넓이 구하기, Time: 0.85 ms, Memory: 31.4 MB -BaekjoonHub

--- 프로그래머스/0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.cs
+++ 프로그래머스/0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.cs
@@ -1,30 +1,18 @@
 using System;
+using System.Linq;
 
 public class Solution {
     public int solution(int[,] dots) {
         int answer = 0;
-        int width = 0;
-        int height = 0;
-        int equal = 0;
-        for(int i = 1; i < dots.GetLength(0); i++)
+        int[] x = new int[4];
+        int[] y = new int[4];
+        
+        for(int i = 0; i < 4; i++)
         {
-            if(dots[0,0] == dots[i,0]){
-                if(dots[0,1] * dots[i,1] < 0) //0을 사이에 둔 좌표
-                {
-                    height = Math.Abs(0 - dots[0,1]) + Math.Abs(0 - dots[i,1]);
-                }else
-                    height = Math.Abs(dots[0,1] - dots[i,1]); //음수 확인 후 절댓값반영
-                equal = i;
-            }else{
-                if(dots[0,0] * dots[i,0] < 0) //0을 사이에 둔 좌표
-                {
-                    width = Math.Abs(0 - dots[0,0]) + Math.Abs(0 - dots[i,0]);
-                }else
-                    width = Math.Abs(dots[0,0] - dots[i,0]);
-                if(equal != 0)
-                    break;
-            }
+            x[i] = dots[i,0];
+            y[i] = dots[i,1];
         }
-        return answer = height * width;
+        answer = (x.Max() - x.Min()) * (y.Max() - y.Min());
+        return answer;
     }
 }
