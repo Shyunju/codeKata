fba4493a53d26c40a5da9bbc18f6ccb54038efba
SHIN HYUNJU <71755508+Shyunju@users.noreply.github.com>
2024-04-29 20:07:08 +0900

    [level 0] Title: 카운트 다운, Time: 0.18 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181899. 카운트 다운/카운트 다운.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class Solution {
+    public int[] solution(int start, int end_num) {
+        int[] answer = new int[start - end_num + 1];
+        for(int i = 0; i < answer.Length; i++)
+        {
+            answer[i] = start - i;
+        }
+        return answer;
+    }
+}
