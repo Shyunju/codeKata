c8970f99384e6be60a354b841c259abe6defdc1c
SHIN HYUNJU <71755508+Shyunju@users.noreply.github.com>
2024-05-03 15:37:03 +0900

    [level 2] Title: 최솟값 만들기, Time: 2.84 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Solution {
+    public int solution(int[] A, int[] B) {
+        int answer = 0;
+        Array.Sort(A);
+        Array.Sort(B);
+        Array.Reverse(B);
+        for(int i = 0; i < A.Length; i++)
+        {
+            answer += A[i] * B[i];
+        }
+        return answer;
+    }
+}
