048b4f0e8503dbee613a85e1d69e9321c0a52c67
SHIN HYUNJU <71755508+Shyunju@users.noreply.github.com>
2024-05-13 15:34:55 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 0.65 ms, Memory: 31.1 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.cs
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.cs
@@ -3,24 +3,20 @@ using System;
 public class Solution {
     public int[] solution(string s) {
         int[] answer = new int[2];
-        int count = 0;
         int length = 0;
-        int turn = 0;
         while(!s.Equals("1"))
         {
-            turn++;
+            answer[0]++;
             length = 0;
             foreach(char i in s)
             {
                 if(i == '0')
-                    count++;
+                    answer[1]++;
                 else
                     length++;
             }
             s = Convert.ToString(length, 2);
         }
-        answer[0] = turn;
-        answer[1] = count;
         return answer;
     }
 }
