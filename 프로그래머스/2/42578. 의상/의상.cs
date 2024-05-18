301da8245fcf0567dd3fa89e6208b99f7be4958c
SHIN HYUNJU <71755508+Shyunju@users.noreply.github.com>
2024-05-18 16:47:47 +0900

    [level 2] Title: 의상, Time: 2.20 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+
+public class Solution {
+    public int solution(string[,] clothes) {
+        int answer = 1;
+        Dictionary<string, int> dic = new Dictionary<string, int>();
+        
+        for(int i = 0 ; i < clothes.GetLength(0); i++)
+        {
+            if(dic.ContainsKey(clothes[i, 1]))
+                dic[clothes[i, 1]]++;
+            else{
+                dic.Add(clothes[i, 1], 1);
+            }
+                
+        }
+        foreach(KeyValuePair<string, int> kv in dic)
+        {
+            answer *= 1 + kv.Value;
+        }
+        return answer -1;
+    }
+}
