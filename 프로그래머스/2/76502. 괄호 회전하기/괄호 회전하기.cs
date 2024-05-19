022d438b6730e0d491987b28d083cbe9326046f4
SHIN HYUNJU <71755508+Shyunju@users.noreply.github.com>
2024-05-19 22:50:49 +0900

    [level 2] Title: 괄호 회전하기, Time: 1.58 ms, Memory: 31.2 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.cs
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.cs
@@ -1,39 +1,41 @@
 using System;
 using System.Collections.Generic;
+
 public class Solution {
     public int solution(string s) {
         int answer = 0;
-        
-        List<char> str = new List<char> (s);
-        for (int i = 0; i < s.Length; i++)
+        List<char> str = new List<char>(s);
+        for(int i = 0; i < s.Length; i++)
         {
-            answer += isProperBrackets(str) ? 1 : 0;
+            answer += isCollect(str) ? 1 : 0;
             str.Add(str[0]);
             str.RemoveAt(0);
         }
-        
         return answer;
     }
-    private bool isProperBrackets(List<char> str)
+    
+    public bool isCollect(List<char> str)
     {
-        Stack<char> brackets = new Stack<char> ();
         string open = "({[";
         string close = ")}]";
         
-        for (int i = 0; i < str.Count; i++)
+        Stack<char> stack = new Stack<char>();
+        
+        for(int i = 0; i <str.Count; i++)
         {
-            if (open.Contains(str[i]))
-                brackets.Push(str[i]);
-            else
-            {
-                if (brackets.Count == 0)
+            if(open.Contains(str[i]))
+                stack.Push(str[i]);
+            else{
+                if(stack.Count == 0)
                     return false;
-                if (str[i] != close[open.IndexOf(brackets.Peek())])
+                if(str[i] != close[open.IndexOf(stack.Peek())])
                     return false;
-                brackets.Pop();
+                
+                stack.Pop();
             }
         }
-        if (brackets.Count != 0)
+        
+        if(stack.Count != 0)
             return false;
         return true;
     }
