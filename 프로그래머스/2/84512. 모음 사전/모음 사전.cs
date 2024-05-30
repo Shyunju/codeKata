f4be0b29325475191ee21a24b7614c1ea1fbddd0
SHIN HYUNJU <71755508+Shyunju@users.noreply.github.com>
2024-05-30 17:01:07 +0900

    [level 2] Title: 모음 사전, Time: 11.60 ms, Memory: 32.9 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.cs
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.cs
@@ -1,22 +1,42 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 public class Solution {
+
+    int count = 0;
+    int result = 0;
+
     public int solution(string word) {
-        int answer = 0;
-        string useWords = "AEIOU";
-        int[] index =new int[useWords.Length];
-        
-        index[0] = 1;
-        for(int i = 0; ++i< index.Length;)
-        {
-            index[i] = index[i -1] * useWords.Length + 1;
-        }
-        Array.Reverse(index);
-        for(int i = -1; ++i < word.Length;)
-        {
-            int wordIndex = useWords.IndexOf(word[i]);
-            answer += wordIndex * index[i] +1;
-        }
-        return answer;
+
+        dfs("", word);
+
+        return result;
     }
+
+    public void dfs(string s, string word)
+        {
+            char[] alphabet = new char[] { 'A', 'E', 'I', 'O', 'U' };
+            Stack<char> stack = new Stack<char>(s.ToArray());
+
+            if (s == word)
+            {
+                result = count;
+                return;
+            }
+
+            if (stack.Count() == 5) return;
+
+            for (int i = 0; i < 5; i++)
+            {
+                stack.Push(alphabet[i]);
+                count++;
+
+                string nextWord = string.Join("", stack.ToArray().Reverse());
+
+                dfs(nextWord, word);                
+
+                stack.Pop();
+            }
+        }
 }
