209b02fdfbae10da5c2c8bf131a1085fa81ac56f
SHIN HYUNJU <71755508+Shyunju@users.noreply.github.com>
2024-06-12 17:17:33 +0900

    [level 1] Title: 신고 결과 받기, Time: 4.70 ms, Memory: 30.9 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.cs
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.cs
@@ -4,13 +4,30 @@ using System.Collections.Generic;
 
 public class Solution {
     public int[] solution(string[] id_list, string[] report, int k) {
-             var tReport = report.Distinct().
-                Select(s => s.Split(' ')).
-                GroupBy(g => g[1]).
-                Where(w => w.Count() >= k).
-                SelectMany(sm => sm.Select(s => s[0])).
-                ToList();
-
-            return id_list.ToDictionary(x => x, x => tReport.Count(c => x == c)).Values.ToArray();
+        if( k > id_list.Length)
+            return new int[id_list.Length];
+        Dictionary<string, int> result = new Dictionary<string, int>();
+        ReportData[] rData = new ReportData[id_list.Length];
+        
+        for(int i = 0; i < id_list.Length; i++){
+            result.Add(id_list[i], 0);
+            rData[i] = new ReportData{name = id_list[i], ReportPeople = new HashSet<string>()};
+        }
+        foreach(string ids in report){
+            string[] id = ids.Split(' ');
+            rData.First(x => x.name == id[1]). ReportPeople.Add(id[0]);
+        }
+        for(int i = 0; i < id_list.Length; i++){
+            if(rData[i].ReportCount < k)
+                continue;
+            foreach(var id in rData[i].ReportPeople)
+                result[id]++;
+        }
+        return result.Select(x => x.Value).ToArray();
     }
+}
+public class ReportData{
+    public string name {get; set;}
+    public int ReportCount {get {return ReportPeople.Count;}}
+    public HashSet<string> ReportPeople {get; set;}
 }
