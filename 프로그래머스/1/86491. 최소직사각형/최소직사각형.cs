1113b011b022008957401ba5b0603ad98e1585d7
SHIN HYUNJU <71755508+Shyunju@users.noreply.github.com>
2024-06-15 22:23:35 +0900

    [level 1] Title: 최소직사각형, Time: 0.53 ms, Memory: 32.1 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.cs
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.cs
@@ -3,13 +3,14 @@ using System;
 public class Solution {
     public int solution(int[,] sizes) {
         int[,] sort = new int[sizes.GetLength(0),sizes.GetLength(1)];
+        
+        int width = 0;
+        int height = 0;
+        
         for(int i= 0 ; i < sizes.GetLength(0); i++){
             sort[i, 0] = Math.Max(sizes[i,0], sizes[i,1]);
             sort[i, 1] = Math.Min(sizes[i,0], sizes[i,1]);
-        }
-        int width = sort[0,0];
-        int height = sort[0,1];
-        for(int i = 1; i < sort.GetLength(0); i++){
+            
             width = Math.Max(width, sort[i,0]);
             height = Math.Max(height, sort[i,1]);
         }
