b76bb4c6b9ff6637e6970622ff59ebd0fa7de7e2
SHIN HYUNJU <71755508+Shyunju@users.noreply.github.com>
2024-06-24 14:29:31 +0900

    [level 2] Title: 호텔 대실, Time: 8.53 ms, Memory: 32.2 MB -BaekjoonHub

--- 프로그래머스/2/155651. 호텔 대실/호텔 대실.cs
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.cs
@@ -1,28 +1,31 @@
 using System;
-
+using System.Collections.Generic;
+using System.Linq;
 public class Solution {
     public int solution(string[,] book_time) {
-        var length = 24 * 60 + 10;
-        int answer = 0;
-
-        var stores = new int[length];
-        for(int i = 0; i < book_time.GetLength(0); i++)
-        {
-            var ss = book_time[i, 0].Split(":");
-            var s = int.Parse(ss[0]) * 60 + int.Parse(ss[1]);
-            var ee = book_time[i, 1].Split(":");
-            var e = int.Parse(ee[0]) * 60 + int.Parse(ee[1]);
-
-
-            stores[s] += 1;
-            stores[e + 10] += -1;
+        List<(int, int)> rental = new List<(int inTime, int outTime)>();
+        
+        for(int i = 0; i < book_time.GetLength(0); i++){
+            int inTime = toTime(book_time[i,0]);
+            int outTime = toTime(book_time[i,1]);
+            
+            rental.Add((inTime, outTime));
         }
-
-        for(int i = 1; i < length; i++)
-        {
-            stores[i] += stores[i - 1];
-            answer = Math.Max(answer, stores[i]);
+        var sorted = rental.OrderBy(o => o.Item1);
+        List<int> roomList = new List<int>();
+        
+        foreach((int inTime, int outTime)t in sorted){
+            int room = roomList.FindIndex(f => f <= t.inTime-10);
+            
+            if( room == -1)
+                roomList.Add(t.outTime);
+            else
+                roomList[room] = t.outTime;            
         }
-        return answer;
-}
+        return roomList.Count;
+    }
+    public int toTime(string time){
+        string[] num = time.Split(':');
+        return int.Parse(num[0]) * 60 + int.Parse(num[1]);
+    }
 }
