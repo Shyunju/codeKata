c2f5c39119a086b43a414c40aa6945935a66efb5
SHIN HYUNJU <71755508+Shyunju@users.noreply.github.com>
2024-06-26 21:19:22 +0900

    [level 2] Title: 점 찍기, Time: 0.19 ms, Memory: 31.6 MB -BaekjoonHub

--- 프로그래머스/2/140107. 점 찍기/점 찍기.cs
+++ 프로그래머스/2/140107. 점 찍기/점 찍기.cs
@@ -3,12 +3,10 @@ using System;
 public class Solution {
     public long solution(int k, int d) {
         long answer = 0;
-        long kk = (long) k*k;
-        long dd = (long) d*d;
+        long dd = (long)d*d;
         
-        for(long x = 0; x <=d ; x +=k){
-            long xx = (long)x*x;
-            long y = (long)Math.Sqrt((dd - xx) / kk);
+        for(long i = 0; i <=d ; i += k){
+            long y = (long)Math.Sqrt(dd - i*i) / k;
             answer += (y +1);
         }
         return answer;
