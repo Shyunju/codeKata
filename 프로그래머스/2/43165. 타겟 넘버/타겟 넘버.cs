7c264f920ae009bd74057f5ad798281cf07374a7
SHIN HYUNJU <71755508+Shyunju@users.noreply.github.com>
2024-07-10 17:37:42 +0900

    [level 2] Title: 타겟 넘버, Time: 0.39 ms, Memory: 31.3 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cs
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cs
@@ -1,23 +1,16 @@
- using System;
+using System;
 
-    public class Solution
-    {
-        public int solution(int[] numbers, int target)
-        {
-            int answer = 0;
-            return Dfs(numbers, target, 0, 0);
-        }
-        
-        static int Dfs(int[] arr, int target, int idx, int num)
-        {
-            if(idx == arr.Length)
-            {
-                if(target == num) return 1;
-                return 0;
-            }
-            return Dfs(arr, target, idx+1, num + arr[idx]) + Dfs(arr, target, idx+1, num - arr[idx]);
-            
-        }
-
-        
+public class Solution {
+    public int solution(int[] numbers, int target) {
+        return DFS(numbers, target, 0, 0);
     }
+    public int DFS(int[] numbers, int target, int idx, int num){
+        if(idx == numbers.Length){
+            if(num == target)
+                return 1;
+            return 0;
+        }
+        
+        return DFS(numbers, target, idx+1, num + numbers[idx])+ DFS(numbers, target, idx+1, num - numbers[idx]);
+    }
+}
