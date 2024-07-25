f3c8ee2d95e520f531da16c27e4fde4226d629f2
SHIN HYUNJU <71755508+Shyunju@users.noreply.github.com>
2024-07-25 21:02:24 +0900

    [level 2] Title: 두 원 사이의 정수 쌍, Time: 20.22 ms, Memory: 31.5 MB -BaekjoonHub

--- 프로그래머스/2/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.cs
+++ 프로그래머스/2/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.cs
@@ -1,32 +1,27 @@
 using System;
 
 public class Solution {
-    public long solution(int r1, int r2) 
-    {
+    public long solution(int r1, int r2) {
         long answer = 0;
-        long rr1 = (long)r1 * (long)r1;
-        long rr2 = (long)r2 * (long)r2;
-
-        for(long x = 0; x < r2; ++x){
-            
-            if(x == 0){
-                answer += r2 - r1 + 1;
-            }
+        long c1 = (long)r1 * (long)r1;
+        long c2 = (long)r2 * (long)r2;
+        
+        for(long x = 0; x < r2; x++){
+            if(x == 0)
+                answer += r2 -r1 +1;
             else if(x >= r1){
-                double y = Math.Sqrt(rr2 - x * x);
+                double y = Math.Sqrt(c2 - x *x);
                 answer += (int)Math.Floor(y);
-            }
-            else {
-                long xx = x * x;
-                double y2 = Math.Sqrt(rr2 - xx);
-                double y1 = Math.Sqrt(rr1 - xx);
-
+            }else{
+                long xx = x*x;
+                double y2 = Math.Sqrt(c2 - xx);
+                double y1 = Math.Sqrt(c1 - xx);
+                
                 int floor = (int)Math.Floor(y2);
                 int ceil = (int)Math.Ceiling(y1);
                 answer += floor - ceil + 1;
             }
         }
-
         return answer * 4;
     }
 }
