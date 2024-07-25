2c49c9834eacd9cf72f7fa4c3a4265bda6d7dfa2
SHIN HYUNJU <71755508+Shyunju@users.noreply.github.com>
2024-07-25 23:26:12 +0900

    [level 2] Title: 과제 진행하기, Time: 7.58 ms, Memory: 32.1 MB -BaekjoonHub

--- 프로그래머스/2/176962. 과제 진행하기/과제 진행하기.cs
+++ 프로그래머스/2/176962. 과제 진행하기/과제 진행하기.cs
@@ -1,62 +1,53 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 public class Solution {
     public string[] solution(string[,] plans) {
-        var answer = new List<string>();
-        var willPlayed = new Test[plans.GetLength(0)];
-        var holded = new Stack<Test>();
-
-        for(int i = 0; i < plans.GetLength(0); ++i)
-            willPlayed[i] = new Test(plans[i, 0], plans[i, 1], plans[i, 2]);
-
-        Array.Sort(willPlayed, (a, b) => a.start < b.start ? -1 : 1);
-
-        int time = 0;
-        int startIndex = 0;
-        while(startIndex < plans.GetLength(0))
-        {
-            Test cur = willPlayed[startIndex];
-
-            bool isLast = startIndex + 1 == plans.GetLength(0);
-            int availableTime = isLast ? int.MaxValue : willPlayed[startIndex + 1].start - cur.start;
-
-            holded.Push(cur);
-            while(holded.Count > 0)
-            {
-                Test top = holded.Peek();
-                if(availableTime >= top.remain) // 시간 충분. 꺼낸 과제완료
-                {
-                    availableTime -= top.remain;
+        List<string> answer = new List<string>();
+        var hold = new Stack<Table>();
+        var homework = new Table[plans.GetLength(0)];
+        
+        for(int i = 0; i < plans.GetLength(0); i++)
+            homework[i] = new Table(plans[i,0], plans[i,1], plans[i,2]);
+        homework = homework.OrderBy(o => o.start).ToArray();
+        
+        int startIdx = 0;
+        
+        while(startIdx < plans.GetLength(0)){
+            Table cur = homework[startIdx];
+            bool isLast = startIdx +1 == plans.GetLength(0);
+            int availableTime = isLast ? int.MaxValue : homework[startIdx + 1].start - cur.start;
+            
+            hold.Push(cur);
+            
+            while(hold.Count > 0){
+                Table top = hold.Peek();
+                
+                if(availableTime >= top.need){
+                    availableTime -= top.need;
                     answer.Add(top.name);
-                    holded.Pop();
-
-                    if(availableTime == 0)
-                        break;
-                }
-                else // 꺼낸 과제를 완료하기에는 시간이 부족함.
-                {
-                    top.remain -= availableTime;
+                    hold.Pop();
+                    
+                    if(availableTime == 0) break;
+                }else{
+                    top.need -= availableTime;
                     break;
                 }
             }
-
-            ++startIndex;
+            ++startIdx;
         }
-
         return answer.ToArray();
     }
 }
-public class Test
-{
+public class Table{
     public string name;
     public int start;
-    public int remain;
-
-    public Test(string name, string timeStr, string playTimeStr)
-    {
+    public int need;
+    
+    public Table(string name, string startTime, string needTime){
         this.name = name;
-        string[] splited = timeStr.Split(':');
-        start = int.Parse(splited[0]) * 60 + int.Parse(splited[1]);
-        remain = int.Parse(playTimeStr);
+        string[] split = startTime.Split(':');
+        start = int.Parse(split[0]) * 60 + int.Parse(split[1]);
+        need = int.Parse(needTime);
     }
 }
