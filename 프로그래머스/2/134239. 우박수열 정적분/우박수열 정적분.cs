ce6d3bfd5c515aa053ec1c834a8326e5ff459614
SHIN HYUNJU <71755508+Shyunju@users.noreply.github.com>
2024-07-31 18:16:11 +0900

    [level 2] Title: 우박수열 정적분, Time: 19.01 ms, Memory: 33.1 MB -BaekjoonHub

--- 프로그래머스/2/134239. 우박수열 정적분/우박수열 정적분.cs
+++ 프로그래머스/2/134239. 우박수열 정적분/우박수열 정적분.cs
@@ -1,43 +1,37 @@
 using System;
 using System.Collections.Generic;
 public class Solution {
-    public double[] solution(int k, int[,] ranges) 
-    {
+    public double[] solution(int k, int[,] ranges) {
+        double[] answer = new double[ranges.GetLength(0)];
+        
         var dots = new List<(int x, int y)>();
-        dots.Add((0, k));
-
+        dots.Add((0,k));
+        
         int i = 1;
-        while(k > 1)
-        {
+        while( k > 1){
             if(k % 2 == 0)
-                k = k / 2;
+                k /= 2;
             else
                 k = k * 3 + 1;
-
+            
             dots.Add((i, k));
-            ++i;
+            i++;
         }
-
-        var answer = new double[ranges.GetLength(0)];
-        for(int j = 0; j < ranges.GetLength(0); ++j)
-        {
-            int left = ranges[j, 0];
-            int right = dots.Count + ranges[j, 1] - 1;
-            if(left > right)
-            {
+        for(int j = 0; j < ranges.GetLength(0); j++){
+            int left = ranges[j,0];
+            int right = dots.Count + ranges[j,1] -1;
+            
+            if(left > right){
                 answer[j] = -1;
                 continue;
             }
-
             double sum = 0d;
-            for(int x = left; x < right; ++x)
-            {
-                sum += Math.Min(dots[x].Item2, dots[x + 1].Item2); // 사각형
-                sum += Math.Abs(dots[x].Item2 - dots[x + 1].Item2) * 0.5d; // 삼각형
+            for(int l = left; l < right; l++){
+                sum += Math.Min(dots[l].Item2, dots[l+1].Item2);
+                sum += Math.Abs(dots[l].Item2 - dots[l+1].Item2) * 0.5d;
             }
             answer[j] = sum;
         }
-
         return answer;
     }
 }
