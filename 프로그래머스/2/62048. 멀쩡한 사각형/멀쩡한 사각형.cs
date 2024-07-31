136e30b620c8688a3b6a716216f59d152145fb42
SHIN HYUNJU <71755508+Shyunju@users.noreply.github.com>
2024-07-31 19:34:50 +0900

    [level 2] Title: 멀쩡한 사각형, Time: 0.21 ms, Memory: 31.2 MB -BaekjoonHub

--- 프로그래머스/2/62048. 멀쩡한 사각형/멀쩡한 사각형.cs
+++ 프로그래머스/2/62048. 멀쩡한 사각형/멀쩡한 사각형.cs
@@ -1,16 +1,15 @@
 using System;
 
-class Solution {
-    public long solution(long w,long h) {
+public class Solution {
+    public long solution(long w, long h) {
         long lgcd = gcd(w, h);
-        long answer = w * h - (w + h - lgcd);
+        long answer = w * h - ( w + h - lgcd);
+        
         return answer;
     }
-
-     public long gcd(long a, long b) {
-        if (b == 0) {
-            return (long)a;
-        }
-        return gcd(b, a%b);
+    public long gcd(long a, long b){
+        if(b == 0)
+            return a;
+        return gcd(b, a % b);
     }
 }
