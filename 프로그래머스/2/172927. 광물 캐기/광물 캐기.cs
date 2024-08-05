2adc60c2132757879f80553b6d93d8f90d98688e
SHIN HYUNJU <71755508+Shyunju@users.noreply.github.com>
2024-08-05 20:19:50 +0900

    [level 2] Title: 광물 캐기, Time: 44.63 ms, Memory: 31.3 MB -BaekjoonHub

--- 프로그래머스/2/172927. 광물 캐기/광물 캐기.cs
+++ 프로그래머스/2/172927. 광물 캐기/광물 캐기.cs
@@ -1,57 +1,43 @@
 using System;
 
 public class Solution {
-    public int solution(int[] picks, string[] minerals) 
-    {
-        int[,] pickCost = new int[3, 3]{ {1, 1, 1}, {5, 1, 1}, {25, 5, 1} };
-        int[] mineralIndexs = new int[minerals.Length];
-        for(int i = 0; i < minerals.Length; ++i)
-        {
-            switch(minerals[i])
-            {
-                case "diamond": mineralIndexs[i] = 0; break;
-                case "iron": mineralIndexs[i] = 1; break;
-                case "stone": mineralIndexs[i] = 2; break;
-                default: break;
+    public int solution(int[] picks, string[] minerals) {
+        int answer = int.MaxValue;
+        int[,] pickCost = new int[3,3] { {1, 1, 1}, { 5, 1, 1}, {25, 5, 1}};
+        int [] mineralIdx = new int[minerals.Length];
+        for(int i = 0; i < minerals.Length; i++){
+            switch(minerals[i]){
+                case "diamond" : mineralIdx[i] = 0; break;
+                case "iron" : mineralIdx[i] = 1; break;
+                case "stone" : mineralIdx[i] = 2; break;
             }
         }
-
-        int answer = int.MaxValue;
-        DFS(picks, mineralIndexs, pickCost, 0, 0, ref answer);
+        dfs(picks, mineralIdx, pickCost, 0, 0, ref answer);
         return answer;
     }
-
-    private void DFS(int[] picks, int[] mineralIndexs, int[,] pickCost, int n, int fatigue, ref int answer)
-    {
-        const int MAX_PICK = 5;
-
-        if(n >= mineralIndexs.Length) // 광물을 다 캐버림
-        {
+    public void dfs(int[] picks, int[] mineralIdx, int[,] pickCost, int n, int fatigue, ref int answer){
+        const int MAX = 5;
+        if ( n >= mineralIdx.Length){
             if(fatigue < answer)
                 answer = fatigue;
         }
-
-        bool isAnyPick = false;
-        for(int i = 0; i < 3; ++i)
-        {
+        bool empty = true;
+        for(int i = 0; i < 3; i++){
             if(picks[i] == 0) continue;
-            isAnyPick = true;
-
+            empty = false;
+            
             int curFatigue = 0;
-            for(int k = 0; k < MAX_PICK; ++k)
-            {
-                if(n + k >= mineralIndexs.Length) break;
-                int mineIndex = mineralIndexs[n + k];
-                curFatigue += pickCost[i, mineIndex];
+            for(int k = 0; k < MAX; k++){
+                if(n + k >= mineralIdx.Length) break;
+                
+                int curMineral = mineralIdx[n+k];
+                curFatigue += pickCost[i, curMineral];
             }
-
             --picks[i];
-            DFS(picks, mineralIndexs, pickCost, n + MAX_PICK, fatigue + curFatigue, ref answer);
+            dfs(picks, mineralIdx, pickCost, n+MAX, fatigue + curFatigue, ref answer);
             ++picks[i];
         }
-
-        if(!isAnyPick) // 곡괭이를 다 써버림
-        {
+        if(empty){
             if(fatigue < answer)
                 answer = fatigue;
         }
