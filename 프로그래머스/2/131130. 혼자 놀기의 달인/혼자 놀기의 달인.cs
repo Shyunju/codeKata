c70ed8f53f2755e1c0c1a4731db2383ef96ef187
SHIN HYUNJU <71755508+Shyunju@users.noreply.github.com>
2024-08-08 21:04:28 +0900

    [level 2] Title: 혼자 놀기의 달인, Time: 0.34 ms, Memory: 31.4 MB -BaekjoonHub

--- 프로그래머스/2/131130. 혼자 놀기의 달인/혼자 놀기의 달인.cs
+++ 프로그래머스/2/131130. 혼자 놀기의 달인/혼자 놀기의 달인.cs
@@ -1,41 +1,33 @@
 using System;
-using System.Linq;
+
 public class Solution {
-    public int solution(int[] cards) 
-    {
+    public int solution(int[] cards) {
         int answer = 0;
-
-        for(int a = 0; a < cards.Length; ++a) // 첫 상자를 완전탐색
-        {
-            bool[] isVisit = Enumerable.Repeat(false, cards.Length).ToArray();
-
-            int openCountA = Check(cards, isVisit, a);
-            if(openCountA == cards.Length) // 모든 상자가 다 열려있으면 0점으로 종료
-                continue;
-
-            for(int b = 0; b < cards.Length; ++b) // 상자를 완전탐색
-            {
+        
+        for(int a = 0; a < cards.Length; a++){
+            bool[] isVisit = new bool[cards.Length];
+            int cntA = Pick(cards, isVisit, a);
+            
+            if(cntA == cards.Length) continue;
+            
+            for(int b = 0; b < cards.Length; b++){
                 if(isVisit[b]) continue;
-
-                int openCountB = Check(cards, isVisit, b);
-                answer = Math.Max(answer, openCountA * openCountB);
+                
+                int cntB = Pick(cards, isVisit, b);
+                answer = Math.Max(answer, cntA * cntB);
             }
         }
-
         return answer;
     }
-
-    private int Check(int[] cards, bool[] isVisit, int i)
-    {
-        int current = cards[i];
-        int openCount = 0;
-        while(!isVisit[current - 1])
-        {
-            isVisit[current - 1] = true;
-            ++openCount;
-            current = cards[current - 1];
+    private int Pick(int[] cards, bool[] isVisit, int i){
+        int cur = cards[i];
+        int cnt = 0;
+        
+        while(!isVisit[cur-1]){
+            isVisit[cur-1] = true;
+            ++cnt;
+            cur = cards[cur-1];
         }
-
-        return openCount;
+        return cnt;
     }
 }
