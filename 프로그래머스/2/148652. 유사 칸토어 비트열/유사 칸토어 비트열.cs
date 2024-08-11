3693f9085e211f44529662f11dd69083e6978bb7
SHIN HYUNJU <71755508+Shyunju@users.noreply.github.com>
2024-08-11 22:12:51 +0900

    [level 2] Title: 유사 칸토어 비트열, Time: 746.37 ms, Memory: 31.5 MB -BaekjoonHub

--- 프로그래머스/2/148652. 유사 칸토어 비트열/유사 칸토어 비트열.cs
+++ 프로그래머스/2/148652. 유사 칸토어 비트열/유사 칸토어 비트열.cs
@@ -1,28 +1,18 @@
- using System;
+using System;
 
-    public class Solution
-    {
-        public int solution(int n, long l, long r)
-        {
-            int answer = 0;
-            
-            // 조건의 폐구간의 모든 부분을 순회
-            for (l = l - 1; l < r; l++)
-                if (Function(l))
-                    answer++;
-
-            return answer;
+public class Solution {
+    public int solution(int n, long l, long r) {
+        int answer = 0;
+        
+        for(l = l -1; l < r; l++){
+            if(Cantor(l)) answer++;
         }
-
-        // 유사 칸토어 비트열이 1이면 true를 반환하는 메서드
-        public bool Function(long num)
-        {
-            if (num % 5 == 2) // "11011" -> "0"
-                return false;
-            else if (num < 5) // "11011" -> "1"
-                return true;
-            
-            // 현재 유사 칸토어 비트열의 레벨에선 0이라고 확신할 수 없을 경우
-            return Function(num / 5);
-        }
+        return answer;
     }
+    private bool Cantor(long num){
+        if( num % 5 == 2) return false;
+        else if(num < 5) return true;
+        
+        return Cantor(num / 5);
+    }
+}
