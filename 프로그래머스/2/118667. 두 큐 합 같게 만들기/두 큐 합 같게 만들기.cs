92b282a81d16a5affb6b92eae5a4a61162c02226
SHIN HYUNJU <71755508+Shyunju@users.noreply.github.com>
2024-08-15 20:25:21 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 42.20 ms, Memory: 50.7 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.cs
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.cs
@@ -1,42 +1,36 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
+using System.Collections.Generic;
 public class Solution {
-        public int solution(int[] queue1, int[] queue2) 
-    {
-        var q1 = new Queue<long>(queue1.Select(s => (long)s)); 
-        var q2 = new Queue<long>(queue2.Select(s => (long)s)); 
-
+    public int solution(int[] queue1, int[] queue2) {
+        int answer = 0;
+        var q1 = new Queue<long>(queue1.Select(s => (long)s));
+        var q2 = new Queue<long>(queue2.Select(s => (long)s));
+                       
+        long maxCnt = queue1.Length * 3;
         long sum1 = q1.Sum();
         long sum2 = q2.Sum();
-
-        if(sum1 == sum2) return 0; // 이미 같다면 수행 필요 없음
-
+        
+        if(sum1 == sum2) return 0;
+        
         long sum = sum1 + sum2;
-        if(sum % 2 == 1) return -1; // 홀수는 같은 값으로 나눌 수 없음
-
-        int maxTryCount = queue1.Length * 3;
+        if(sum % 2 == 1) return -1;
+        
         long target = sum / (long)2;
-        int answer = 0;
-        while(sum1 != target)
-        {
-            if(answer > maxTryCount) // 무한루프 방지
-                return -1;
-
-            if(sum1 < target)
-            {
+        
+        while(sum1 != target){
+            if(answer >= maxCnt) return -1;
+            
+            if(sum1 < target){
                 long num = q2.Dequeue();
-                q1.Enqueue(num);
                 sum1 += num;
-                ++answer;
-            }
-            else
-            {
+                q1.Enqueue(num);
+            }else{
                 long num = q1.Dequeue();
-                q2.Enqueue(num);
                 sum1 -= num;
-                ++answer;
+                q2.Enqueue(num);
             }
+            ++answer;
         }
         return answer;
     }
