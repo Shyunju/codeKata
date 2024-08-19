58314999b478ae943ec833e920bd0b676a7e88cc
SHIN HYUNJU <71755508+Shyunju@users.noreply.github.com>
2024-08-19 16:53:08 +0900

    [level 2] Title: 당구 연습, Time: 1.01 ms, Memory: 31 MB -BaekjoonHub

--- 프로그래머스/2/169198. 당구 연습/당구 연습.cs
+++ 프로그래머스/2/169198. 당구 연습/당구 연습.cs
@@ -1,73 +1,61 @@
 using System;
 
-    public class Solution
+public class Solution
+{
+    public int[] solution(int m, int n, int startX, int startY, int[,] balls)
     {
-        public class Pos
+        int[] answer = new int[balls.GetLength(0)];
+        (int y, int x) start = (startY, startX);
+        
+        for (int i = 0; i < balls.GetLength(0); i++)
         {
-            public int y;
-            public int x;
+            int posY = balls[i, 1];
+            int posX = balls[i, 0];
+            (int y, int x) move = (0, 0);
+            int minDir = int.MaxValue;
 
-            public Pos(int y, int x)
+            // 오른쪽 벽에 대한 대칭 좌표 ( y = 2m - y )
+            if ((posX > start.x && posY == start.y) == false)
             {
-                this.y = y;
-                this.x = x;
+                move.y = posY;
+                move.x = 2 * m - posX;
+                minDir = Math.Min(minDir, GetSquareDir(start, move));
             }
-        }
-
-        public int[] solution(int m, int n, int startX, int startY, int[,] balls)
-        {
-            int[] answer = new int[balls.GetLength(0)];
-            Pos startPos = new Pos(startY, startX); // 시작점
-            
-            // 각 좌표 하나하나에 대해 최소 거리를 구함
-            for (int i = 0; i < balls.GetLength(0); i++)
+           // 위쪽 벽에 대한 대칭 좌표 ( x = 2n - x )
+            if ((posY > start.y && posX == start.x) == false)
             {
-                // 맞춰야 하는 공의 좌표
-                int posY = balls[i, 1];
-                int posX = balls[i, 0];
-                Pos movePos;
-                int minDir = int.MaxValue;
-
-                // 오른쪽 벽에 대한 대칭 좌표 ( y = 2m - y )
-                if ((posX > startPos.x && posY == startPos.y) == false)
-                {
-                    movePos = new Pos(posY, 2 * m - posX);
-                    minDir = Math.Min(minDir, GetSquareDir(startPos, movePos));
-                }
-
-                // 위쪽 벽에 대한 대칭 좌표 ( x = 2n - x )
-                if ((posY > startPos.y && posX == startPos.x) == false)
-                {
-                    movePos = new Pos(2 * n - posY, posX);
-                    minDir = Math.Min(minDir, GetSquareDir(startPos, movePos));
-                }
-
+                move.y = 2 * n - posY;
+                move.x = posX;
+                minDir = Math.Min(minDir, GetSquareDir(start, move));
+            }
                 // 아래쪽 벽에 대한 대칭 좌표 ( y *= -1 )
-                if ((posY < startPos.y && posX == startPos.x) == false)
-                {
-                    movePos = new Pos(posY * -1, posX);
-                    minDir = Math.Min(minDir, GetSquareDir(startPos, movePos));
-                }
-
-                // 왼쪽 벽에 대한 대칭 좌표 ( x *= -1 )
-                if ((posX < startPos.x && posY == startPos.y) == false)
-                {
-                    movePos = new Pos(posY, posX * -1);
-                    minDir = Math.Min(minDir, GetSquareDir(startPos, movePos));
-                }
-
-                answer[i] = minDir;
+            if ((posY < start.y && posX == start.x) == false)
+            {
+                move.y = posY * -1;
+                move.x = posX;
+                minDir = Math.Min(minDir, GetSquareDir(start, move));
             }
 
-            return answer;
+            // 왼쪽 벽에 대한 대칭 좌표 ( x *= -1 )
+            if ((posX < start.x && posY == start.y) == false)
+            {
+                move.y = posY;
+                move.x = posX * -1;
+                minDir = Math.Min(minDir, GetSquareDir(start, move));
+            }
+    
+            answer[i] = minDir;
         }
 
-        // 두 점 사이의 거리의 제곱을 리턴하는 메서드
-        public int GetSquareDir(Pos p1, Pos p2)
-        {
-            int dirY = p1.y - p2.y;
-            int dirX = p1.x - p2.x;
-
-            return (dirY * dirY) + (dirX * dirX);
-        }
+        return answer;
     }
+
+    // 두 점 사이의 거리의 제곱을 리턴하는 메서드
+    public int GetSquareDir((int y, int x)p1, (int y, int x)p2)
+    {
+        int dirY = p1.y - p2.y;
+        int dirX = p1.x - p2.x;
+
+        return (dirY * dirY) + (dirX * dirX);
+    }
+}
