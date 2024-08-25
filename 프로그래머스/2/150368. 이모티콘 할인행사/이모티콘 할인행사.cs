4b9ca20ed77cb36393337d03cf8fe8c92e67d088
SHIN HYUNJU <71755508+Shyunju@users.noreply.github.com>
2024-08-25 18:59:22 +0900

    [level 2] Title: 이모티콘 할인행사, Time: 380.55 ms, Memory: 34.9 MB -BaekjoonHub

--- 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.cs
+++ 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.cs
@@ -1,72 +1,55 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
-
+using System.Collections.Generic;
 public class Solution {
-    public class User
-    {
+    public class User{
         public int ratio;
         public int over;
     }
-
-    public class Emoticon
-    {
-        public int originPrice;
-        public int saleRatio; // 10, 20, 30, 40
-        public int GetSalePrice() => originPrice * (100 - saleRatio) / 100;
+    public class Emoticon {
+        public int origin;
+        public int persent;
+        public int GetSalePrice() => origin * (100 - persent) / 100; 
     }
-
-    public int[] solution(int[,] users, int[] emoticons) 
-    {
+    public int[] solution(int[,] users, int[] emoticons) {
         User[] arr = Enumerable.Range(0, users.GetLength(0))
-                            .Select(s => new User{ ratio = users[s, 0], over = users[s, 1] })
-                            .ToArray();
-
-        Emoticon[] emo = emoticons.Select(s => new Emoticon{ originPrice = s })
-                                  .ToArray();
-
-        int plusCount = 0;
+            .Select(s => new User{ratio = users[s,0], over = users[s,1]}).ToArray();
+        
+        Emoticon[] emo = emoticons.Select(s => new Emoticon{origin = s}).ToArray();
+        
+        int joinPlus = 0;
         int sales = 0;
-        DFS(0, arr, emo, ref plusCount, ref sales);
-        return new int[2] { plusCount, sales };
+        DFS(0, arr, emo, ref joinPlus, ref sales);
+        return new int[2]{joinPlus, sales};
     }
-
-    void DFS(int index, User[] users, Emoticon[] emo, ref int plusCount, ref int sales)
-    {
-        if(index == emo.Length)
-        {
-            int curPlusCount = 0;
+    void DFS(int idx, User[] users, Emoticon[] emo, ref int joinPlus, ref int sales){
+        if(idx == emo.Length){
+            int curJoinPlus = 0;
             int curSales = 0;
-            foreach(User user in users)
-            {
-                int userSpendMoney = emo.Where(w => w.saleRatio >= user.ratio)
-                                       .Select(s => s.GetSalePrice())
-                                       .Sum();
-
-                if(userSpendMoney >= user.over) 
-                    ++curPlusCount;
-                else 
-                    curSales += userSpendMoney;
+            
+            foreach(User user in users){
+                int total = emo.Where(w => w.persent >= user.ratio)
+                    .Select(s => s.GetSalePrice())
+                    .Sum();
+                
+                if(total >= user.over)
+                    ++curJoinPlus;
+                else
+                    curSales += total;
             }
-
-            if(plusCount < curPlusCount)
-            {
-                plusCount = curPlusCount;
+            
+            if(joinPlus < curJoinPlus){
+                joinPlus = curJoinPlus;
                 sales = curSales;
-            }
-            else if(plusCount == curPlusCount)
-            {
+            }else if(joinPlus == curJoinPlus){
                 if(sales < curSales)
                     sales = curSales;
             }
-
             return;
         }
-
-        for(int k = 10; k <= 40; k += 10) // 할인 폭은 10% ~ 40% 고정임
-        {
-            emo[index].saleRatio = k; // 할인
-            DFS(index + 1, users, emo, ref plusCount, ref sales);
+        for(int k = 10; k <= 40; k += 10){
+            emo[idx].persent = k;
+            DFS(idx +1, users, emo, ref joinPlus, ref sales);
         }
     }
 }
