ee4a91745d0fb0e84eb15ee65e4a0db25dc144e8
SHIN HYUNJU <71755508+Shyunju@users.noreply.github.com>
2024-09-03 17:01:18 +0900

    [level 3] Title: 단어 변환, Time: 0.80 ms, Memory: 31.1 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.cs
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.cs
@@ -1,62 +1,35 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 public class Solution {
-    string tar;
-    int answer = 0;
-    
-    //dfs
-    //비교 항목, 단계, 방문 여부, 비교 대상 배열
-    public void dfs(string begin, int phase, bool[] visit, string[] words)
-    {
-        //비교 대상 길이만큼 반복
-        for(int i=0; i<words.Length; i++)
-        {
-            //해당 visit 변수가 거짓이면
-            if(!visit[i])
-            {
-                //비교 항목과 [i]번째 비교 대상 단어와 비교
-                int n = 0;
-                for(int j=0; j<words[i].Length; j++)
-                {
-                    //한 글자씩 비교하여 같으면 n변수 더하기
-                    if(begin[j]==words[i][j])
-                    {
-                        n++;
-                    }
-                }
-                //변수 n이 비교 항목 문자열 길이 - 1 과 같을 때
-                if(n==begin.Length-1)
-                {
-                    //비교 항목을 새로운 비교 대상 단어로 치환
-                    begin = words[i];
-                    //해당 번째 방문 true
-                    visit[i]=true;
-                    
-                    //치환된 비교 항목이 목표 타겟과 같으면 dfs 종료
-                    if(begin==tar)
-                    {
-                        answer = phase + 1;
-                    }
-                    //아니면 dfs 실행
-                    else
-                    {
-                        dfs(begin, phase + 1, visit, words);
-                    }
-                }
-            }
-        }
+    public int solution(string begin, string target, string[] words) {
+        if(Array.IndexOf(words, target) == -1) return 0;
+        int answer = int.MaxValue;
+        
+        bool[] visit = new bool[words.Length];
+        dfs(0, begin, target, words, visit, ref answer);
+        
+        return answer == int.MaxValue ? 0 : answer;
     }
     
-    public int solution(string begin, string target, string[] words) {
-        //목표 타겟 변수 저장
-        tar = target;
+    private void dfs(int n, string cur, string target, string[] words, bool[] visit, ref int answer){
         
-        //방문 변수 선언
-        bool[] visit = new bool[words.Length];
+        if(cur == target){
+            if(n < answer) answer = n;
+            return;
+        }
         
-        //dfs 실행
-        dfs(begin, 0, visit, words);
-        
-        return answer;
+        for(int i = 0; i < words.Length; i++){
+            if(visit[i]) continue;
+            if(DiffCount(cur, words[i]) != 1) continue;
+            
+            visit[i] = true;
+            dfs(n+1, words[i], target, words, visit, ref answer);
+            visit[i] = false;
+        }
+    }
+    private int DiffCount(string a, string b){
+        return Enumerable.Range(0, a.Length).Count( c => a[c] != b[c]);
     }
 }
