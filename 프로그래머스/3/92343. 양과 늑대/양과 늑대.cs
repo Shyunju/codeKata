c5e093e2d9f340104e97a0f1a0b0526afccd2db1
SHIN HYUNJU <71755508+Shyunju@users.noreply.github.com>
2024-09-19 20:08:40 +0900

    [level 3] Title: 양과 늑대, Time: 2.77 ms, Memory: 31 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.cs
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.cs
@@ -1,42 +1,38 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
-
+using System.Collections.Generic;
 public class Solution {
     bool[] visited;
-    int[,] edges;
     int[] info;
-    HashSet<int> sheepCounts;
+    int[,] edges;
+    HashSet<int> sheepCnt;
     public int solution(int[] info, int[,] edges) {
         this.info = info;
         this.edges = edges;
-        sheepCounts = new HashSet<int>();
         visited = new bool[info.Length];
+        sheepCnt = new HashSet<int>();
         
         visited[0] = true;
-        DFS(1, 0);
+        DFS(1,0);
         
-        return sheepCounts.Max();
+        return sheepCnt.Max();
     }
-    
-    private void DFS(int sheep, int wolf)
-    {  
-        if (wolf >= sheep) return; // wolf가 많아지는 경우 그 뒤 sheep 만나면 무조건 먹히므로 pass
-        sheepCounts.Add(sheep); // 현재 sheep count 저장
+    private void DFS(int sheep, int wolf){
+        if(wolf >= sheep) return;
         
-        for (int i = 0; i < edges.GetLength(0); i++)
-        {
-            int parent = edges[i, 0];
-            int child = edges[i, 1];
-            int wolfCount = info[child];
-            // 부모 방문하고 아직 자식 방문 안한 경우에 대한 케이스 모두 확인
-            if (visited[parent] && visited[child] == false)
-            {
+        sheepCnt.Add(sheep);
+        
+        for(int i = 0; i < edges.GetLength(0); i++){
+            
+            int parent = edges[i,0];
+            int child = edges[i,1];
+            int plusWolf = info[child];
+            
+            if(visited[parent] && !visited[child]){
                 visited[child] = true;
-                DFS(sheep + 1 - wolfCount, wolf + wolfCount); // dfs로 돈 다음
-                visited[child] = false; // 방문 표시 빼기 
+                DFS(sheep + 1 - plusWolf, wolf + plusWolf);
+                visited[child] = false;
             }
         }
-        
     }
 }
