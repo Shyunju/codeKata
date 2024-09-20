2b6d4f85adda008497d840c721159612d4f7ec07
SHIN HYUNJU <71755508+Shyunju@users.noreply.github.com>
2024-09-20 13:30:01 +0900

    [level 3] Title: 기지국 설치, Time: 0.31 ms, Memory: 31.7 MB -BaekjoonHub

--- 프로그래머스/3/12979. 기지국 설치/기지국 설치.cs
+++ 프로그래머스/3/12979. 기지국 설치/기지국 설치.cs
@@ -2,24 +2,21 @@ using System;
 
 class Solution
 {
-    public int solution(int n, int[] stations, int w)
-    {
-        int answer = 0;
-        int station = 0;
-        int now = 1;
-        while (now <= n)
-        {
-            if (station < stations.Length && now >= stations[station] - w)
-            {
-                now = stations[station] + w + 1;
-                station++;
-            }
-            else
-            {
-                now += w + w + 1;
-                answer++;
-            }
+    public int solution(int n, int[] stations, int w){
+        int answer =0;
+        int width = 2 * w +1;
+        int idx = 1;
+        
+        foreach(int i in stations){
+            int blank = i - w - idx;
+            if(blank > 0)
+                answer += (blank + width - 1) / width;
+            idx = i + w + 1;
         }
+        int last = n + 1 - idx;
+        if(last > 0)
+            answer += (last + width -1) / width;
+
         return answer;
     }
 }
