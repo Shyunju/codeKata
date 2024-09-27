3e2ff8ba0cd2982615c1914355137635618347fc
SHIN HYUNJU <71755508+Shyunju@users.noreply.github.com>
2024-09-27 19:58:45 +0900

    [level 3] Title: 가장 먼 노드, Time: 6808.37 ms, Memory: 390 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.cs
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.cs
@@ -3,38 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 public class Solution {
     public int solution(int n, int[,] edge) {
-        Queue<Tuple<int,int>> queue = new Queue<Tuple<int,int>>();
-            int[] visited = new int[n+1];
-            queue.Enqueue(new Tuple<int,int>(0,1));
-            visited[1]=1;
-            int[,] graph= new int[n+1,n+1];
-            for (int i = 0; i < edge.GetLength(0); i++)
-            {
-                graph[edge[i, 0], edge[i, 1]] = 1;
-                graph[edge[i, 1], edge[i, 0]] = 1;
-            }
-            int answer = 0;
-            int[] distance = new int[n+1];
-            for (int i = 2; i < distance.Length;i++)
-                distance[i] = 50001;
-            while (queue.Count > 0)
-            {
-                Tuple<int,int> t = queue.Dequeue();
-                distance[t.Item2] = distance[t.Item2] > distance[t.Item1] + 1 ? distance[t.Item1] + 1 : distance[t.Item2];
-                for (int i = 1; i < n + 1; i++)
-                {
-                    if (visited[i]!=1 && graph[t.Item2, i] == 1)
-                    {
-                        queue.Enqueue(new Tuple<int,int>(t.Item2,i));
-                        visited[i]=1;
-                    }
+        int answer = 0;
+        int[] visited = new int[n+1];
+        int[,] graph = new int[n+1, n+1];
+        for(int i = 0; i < edge.GetLength(0); i++){
+            graph[edge[i,0], edge[i,1]] = 1;
+            graph[edge[i,1], edge[i,0]] = 1;
+        }
+        int[] dist = new int[n+1];
+        Queue<Tuple<int, int>> q = new Queue<Tuple<int, int>>();
+        q.Enqueue(new Tuple<int, int>(0,1));
+        for(int i = 2; i < n+1; i++){
+            dist[i] = 50001;
+        }
+        while(q.Count > 0){
+            Tuple<int, int> t = q.Dequeue();
+            dist[t.Item2] = dist[t.Item2] > dist[t.Item1] + 1 ? dist[t.Item1] + 1 : dist[t.Item2];
+            
+            for(int i = 1; i < n+1; i++){
+                if(visited[i] == 0 && graph[t.Item2, i] == 1){
+                    q.Enqueue(new Tuple<int, int>(t.Item2, i));
+                    visited[i] = 1;
                 }
             }
-            for (int i = 0; i < distance.Length; i++)
-            {
-                if (distance[i] == distance.Max())
-                    answer++;
-            }
-                return answer;
+        }
+        for(int i = 0; i < dist.Length; i++){
+            if(dist[i] == dist.Max())   answer++;
+        }
+        return answer;
     }
 }
