6e68568d65571a8779d6f268abd2d641cc90b0d3
SHIN HYUNJU <71755508+Shyunju@users.noreply.github.com>
2024-10-16 19:10:18 +0900

    [level 3] Title: 인사고과, Time: 49.33 ms, Memory: 43.3 MB -BaekjoonHub

--- 프로그래머스/3/152995. 인사고과/인사고과.cs
+++ 프로그래머스/3/152995. 인사고과/인사고과.cs
@@ -1,33 +1,28 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
-
 public class Solution {
     public int solution(int[,] scores) {
         int answer = 1;
+        int myScore = scores[0,0] + scores[0,1];
+        var scoreList = new List<(int, int)>();
         
-        List<(int, int)> scoreList = new List<(int, int)>();
-        
-        for (int i = 1; i < scores.GetLength(0); i++)
-        {
-            scoreList.Add((scores[i, 0], scores[i, 1]));
+        for(int i = 0; i < scores.GetLength(0); i++){
+            scoreList.Add((scores[i,0], scores[i,1]));
         }
         
-        scoreList = scoreList.OrderByDescending(x => x.Item1).ThenBy(x => x.Item2).ToList();
-
-        int currentScore = scores[0, 0] + scores[0, 1];
-        int maxEmployeeEvalScore = 0;
-        foreach (var score in scoreList)
-        {
-            if (scores[0, 0] < score.Item1 && scores[0, 1] < score.Item2) return -1;
-            if (maxEmployeeEvalScore <= score.Item2) 
-            {
-                if (currentScore < score.Item1 + score.Item2) answer += 1;
-                maxEmployeeEvalScore = score.Item2;
+        scoreList.Sort(delegate ((int, int)a, (int, int)b){
+            if(a.Item1 == b.Item1)  return a.Item2.CompareTo(b.Item2);
+            return b.Item1.CompareTo(a.Item1);
+        });
+        
+        int max2Score = 0;
+        foreach((int, int)i in scoreList){
+            if(scores[0, 0] < i.Item1 && scores[0,1] < i.Item2)  return -1;
+            if(i.Item2 >= max2Score){
+                if(myScore < i.Item1 + i.Item2) answer++;
+                max2Score = i.Item2;
             }
-            
         }
-        
         return answer;
     }
 }
