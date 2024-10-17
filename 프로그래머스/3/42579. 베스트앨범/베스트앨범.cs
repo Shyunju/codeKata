cba6b528ef66b5456486eb4aae57d436ef291848
SHIN HYUNJU <71755508+Shyunju@users.noreply.github.com>
2024-10-17 19:03:39 +0900

    [level 3] Title: 베스트앨범, Time: 20.13 ms, Memory: 31.6 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.cs
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.cs
@@ -1,22 +1,27 @@
 using System;
-using System.Linq;
 using System.Collections.Generic;
+using System.Linq;
 public class Solution {
-    public int[] solution(string[] genres, int[] plays) 
-    {
+    public int[] solution(string[] genres, int[] plays) {
         var zipList = Enumerable.Range(0, genres.Length)
-                                .Select(i => new{ index = i, genre = genres[i], play = plays[i]})
-                                .ToList();
-
+            .Select(s => new{index = s, genre = genres[s], play = plays[s]})
+            .ToList();
         List<string> genreRank = zipList.GroupBy(g => g.genre)
-                                        .OrderByDescending(g => g.Sum(t => t.play))
-                                        .Select(g => g.Key)
-                                        .ToList();
-
-        var topTwoEnumerable = zipList.GroupBy(g => g.genre)
-                                      .OrderBy(g => genreRank.IndexOf(g.Key))
-                                      .Select(g => g.OrderByDescending(t => t.play).Take(2));
-
-        return topTwoEnumerable.SelectMany(x => x).Select(x => x.index).ToArray();
+            .Select( s => new{genre = s.Key, sum = s.Sum(t => t.play)})
+            .OrderByDescending(o => o.sum)
+            .Select( s => s.genre)
+            .ToList();
+        
+        var topTwo = zipList.GroupBy(g => g.genre)
+            .OrderBy(o => genreRank.IndexOf(o.Key))
+            .Select(s => s.OrderByDescending(t => t.play).Take(2));
+        
+        var list = new List<int>();
+        foreach(var i in topTwo){
+            foreach(var member in i){
+                list.Add(member.index);
+            }
+        }
+        return list.ToArray();
     }
 }
