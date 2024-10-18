d52e46ffa3aaf6aae004a5b30e678a3538ed7943
SHIN HYUNJU <71755508+Shyunju@users.noreply.github.com>
2024-10-18 15:24:42 +0900

    [level 3] Title: 부대복귀, Time: 430.97 ms, Memory: 104 MB -BaekjoonHub

--- 프로그래머스/3/132266. 부대복귀/부대복귀.cs
+++ 프로그래머스/3/132266. 부대복귀/부대복귀.cs
@@ -1,67 +1,38 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 
 public class Solution {
     public int[] solution(int n, int[,] roads, int[] sources, int destination) {
         int[] answer = new int[sources.Length];
+        var q = new Queue<(int, int)>();
+        var dict = new Dictionary<int, List<int>>();
+        bool[] visit = new bool[n+1];
         
-        List<int>[] graph = new List<int>[n+1];
-        // graph 초기화
-        for (int i = 1; i <= n; i++)
-        {
-            graph[i] = new List<int>();
+        for(int i = 0; i < answer.Length; i++)
+            answer[i] = -1;
+        
+        for(int i = 0; i <= n; i++){
+            dict.Add(i, new List<int>());
         }
-        for (int i = 0; i < roads.GetLength(0); i++)
-        {
-            int road1 = roads[i, 0];
-            int road2 = roads[i, 1];
-            graph[road1].Add(road2);
-            graph[road2].Add(road1);
-        }
-        
-        int[] distFromDest = Enumerable.Repeat(100001, n+1).ToArray();
-        // dest 기준 BFS로 dist 구하기
-        // node Num, curr Dist
-        Queue<(int, int)> queue = new Queue<(int, int)>();
-        queue.Enqueue((destination, 0));
-        bool[] visited = new bool[n + 1];
-        visited[destination] = true;
-        distFromDest[destination] = 0;
-        
-        while (queue.Count >= 1)
-        {
-            (int currNode, int dist) = queue.Dequeue();
-            distFromDest[currNode] = Math.Min(dist, distFromDest[currNode]);
-            visited[currNode] = true;
-            
-            foreach(int adjNode in graph[currNode])
-            {
-                if (visited[adjNode]) continue;
-                queue.Enqueue((adjNode, dist + 1));
-            }   
+        for(int i = 0; i < roads.GetLength(0); i++){
+            dict[roads[i,0]].Add(roads[i,1]);
+            dict[roads[i,1]].Add(roads[i,0]);
         }
         
-        for (int i = 0; i < sources.Length; i++)
-        {
-            int source = sources[i];
-            
-            if (graph[source].Count == 0) 
-            {
-                answer[i] = -1;
-                continue;
+        q.Enqueue((destination, 0));
+        visit[destination] = true;
+        
+        while(q.Count > 0){
+            (int now, int count) = q.Dequeue();
+            for(int i= 0; i < dict[now].Count; i++){
+                if(visit[dict[now][i]]) continue;
+                visit[dict[now][i]] = true;
+                q.Enqueue((dict[now][i], count+1));
             }
-            
-            if (distFromDest[source] == 100001)
-            {
-                answer[i] = -1;
-                continue;
+            for(int i = 0; i < sources.Length; i++){
+                if(now == sources[i]) answer[i] = count;
             }
-            
-            answer[i] = distFromDest[source];
         }
-        
-        
         return answer;
     }
 }
