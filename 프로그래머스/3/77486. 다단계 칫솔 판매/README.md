c18db3961f48481ddf7124aec3e464e41939162a
SHIN HYUNJU <71755508+Shyunju@users.noreply.github.com>
2024-10-28 21:25:54 +0900

    [level 3] Title: 다단계 칫솔 판매, Time: 188.68 ms, Memory: 58.9 MB -BaekjoonHub

--- 프로그래머스/3/77486. 다단계 칫솔 판매/README.md
+++ 프로그래머스/3/77486. 다단계 칫솔 판매/README.md
@@ -1,10 +1,10 @@
 # [level 3] 다단계 칫솔 판매 - 77486 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/77486?language=csharp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/77486) 
 
 ### 성능 요약
 
-메모리: 58.8 MB, 시간: 169.21 ms
+메모리: 58.9 MB, 시간: 188.68 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 27일 18:19:50
+2024년 10월 28일 21:25:52
 
 ### 문제 설명
 
--- 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.cs
+++ 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.cs
@@ -2,32 +2,25 @@ using System;
 using System.Linq;
 using System.Collections.Generic;
 public class Solution {
-    public int[] solution(string[] enroll, string[] referral, string[] seller, int[] amount) 
-{
-    var enrollDict = Enumerable.Range(0, enroll.Length)
-                               .ToDictionary(e => enroll[e], e => referral[e]);
-
-    var moneyDict = Enumerable.Range(0, enroll.Length)
-                               .ToDictionary(e => enroll[e], e => 0);
-
-    for(int i = 0; i < seller.Length; ++i)
-    {
-        string s = seller[i];
-        int money = amount[i] * 100;
-
-        while(true)
-        {
-            int tenPercent = (int)Math.Floor(money * 0.1);
-            moneyDict[s] += money - tenPercent; // 자기자신에게 90% 배분
-
-            if(tenPercent == 0) break;
-            if(enrollDict[s] == "-") break;
-
-            s = enrollDict[s];
-            money = tenPercent;
+    public int[] solution(string[] enroll, string[] referral, string[] seller, int[] amount) {
+        var enrollDict = Enumerable.Range(0, enroll.Length).ToDictionary(e => enroll[e], e => referral[e]);
+        var moneyDict = Enumerable.Range(0, enroll.Length).ToDictionary(e => enroll[e], e => 0);
+        
+        for(int i = 0; i < seller.Length; i++){
+            string name = seller[i];
+            int money = amount[i] * 100;
+            
+            while(true){
+                int tenpercent = (int)Math.Floor(money * 0.1);
+                moneyDict[name] += money - tenpercent;
+                
+                if(tenpercent == 0) break;
+                if(enrollDict[name] == "-") break;
+                
+                name = enrollDict[name];
+                money = tenpercent;
+            }
         }
+        return enroll.Select(s => moneyDict[s]).ToArray();
     }
-
-    return enroll.Select(e => moneyDict[e]).ToArray();
-}
 }
