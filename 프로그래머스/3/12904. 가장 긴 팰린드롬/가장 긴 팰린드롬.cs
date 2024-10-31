03c7eeda4ae2fa151c9c92c28b0b093d921b1246
SHIN HYUNJU <71755508+Shyunju@users.noreply.github.com>
2024-10-31 21:19:02 +0900

    [level 3] Title: 가장 긴 팰린드롬, Time: 5.70 ms, Memory: 31.4 MB -BaekjoonHub

--- 프로그래머스/3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.cs
+++ 프로그래머스/3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.cs
@@ -1,26 +1,35 @@
-using System;
 public class Solution {
-    public int solution(string s) {
-        int answer = 0;
-        int[,] dp = new int[2501,2501];
-        char[] arr = new char[2501];
-        
-        for(int i = 0; i < s.Length; i++)
-            arr[i+1] = s[i];
-        
-        for(int i = s.Length; i > 0; i--){
-            for(int j = i; j <= s.Length; j++){
-                if(i == j) dp[i,j] = 1;
-                else if(arr[i] == arr[j] && dp[i+1, j-1] != -1) 
-                    dp[i,j] = dp[i+1, j-1] +2;
-                else
-                    dp[i,j] = -1;
-                
-                answer = Math.Max(answer,dp[i,j]);
+    public int solution(string s) 
+{
+    int answer = 1;
 
-            }
-        }
+    for(int i = 0; i < s.Length - 1; ++i)
+    {
+        // 홀수 구성 체크 aba
+        Check(s, 1, i, i, ref answer);
 
-        return answer;
+        // 짝수 구성 체크 abba
+        if(s[i] == s[i + 1])
+            Check(s, 2, i, i + 1, ref answer);
     }
+
+    return answer;
+}
+
+void Check(string s, int startCount, int left, int right, ref int answer)
+{            
+    int count = startCount;
+
+    while(true)
+    {
+        if(--left < 0) break;
+        if(++right >= s.Length) break;
+        if(s[left] != s[right]) break;
+        count += 2;
+    }
+
+    if(count > answer)
+        answer = count;
+}
+
 }
