2be9a509a86d2bc35609e5bb6e28a8109ee76f67
SHIN HYUNJU <71755508+Shyunju@users.noreply.github.com>
2024-11-01 16:10:08 +0900

    [level 3] Title: 연속 펄스 부분 수열의 합, Time: 21.02 ms, Memory: 50 MB -BaekjoonHub

---
+++ 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Linq;
+
+public class Solution
+{
+    public long GetMaxAccValue(ref int[] intArray)
+    {
+        long[] dp = new long[intArray.Length];
+        
+        dp[0] = Math.Max(0, intArray[0]);
+        for(int i = 1; i < dp.Length; i++)
+            dp[i] = Math.Max(0, dp[i - 1]) + intArray[i];
+        
+        return dp.Max();
+    }
+    
+    public long solution(int[] sequence)
+    {
+        int[] sequence2 = (int[])sequence.Clone();
+        bool isCheck = false;
+        
+        for(int i = 0; i < sequence.Length; i++)
+        {
+            if(isCheck)
+                sequence[i] *= -1;
+            else
+                sequence2[i] *= -1;
+            
+            isCheck = !isCheck;
+        }
+        
+        return Math.Max(GetMaxAccValue(ref sequence), GetMaxAccValue(ref sequence2));
+    }
+}
