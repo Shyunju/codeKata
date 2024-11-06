c98562fd36604cff9ac13b7289575c03df53a03f
SHIN HYUNJU <71755508+Shyunju@users.noreply.github.com>
2024-11-06 20:18:07 +0900

    [level 3] Title: 아방가르드 타일링, Time: 8784.63 ms, Memory: 31.7 MB -BaekjoonHub

--- 프로그래머스/3/181186. 아방가르드 타일링/아방가르드 타일링.cs
+++ 프로그래머스/3/181186. 아방가르드 타일링/아방가르드 타일링.cs
@@ -1,61 +1,27 @@
 using System;
 
-class Solution {
+public class Solution {
     public int solution(int n) {
-        long[] basic = new long[4];
-        long[] dp = new long[100_001];
-        
+        long[] dp = new long[n+2];
+        long per = 1000000007;
         dp[0] = 1;
         dp[1] = 1;
         dp[2] = 3;
-        dp[3] = 10;
-        basic[0] = 1;
-        basic[1] = 1;
-        basic[2] = 2;
-        basic[3] = 5;
-   
-        // save the sum of dp[] for cases
-        long[] patternsOfFourTwo = new long[6];
         
-        for (int i = 4; i <= n; i++) {
-            long four = 0;
-            long two = 0;
-            
-            for (int j = 1; j < 4; j++) {
-                dp[i] += dp[i - j] * basic[j];
-            }
-            
-            if (i > 3) {
-                
-                if (i % 3 == 1) {
-                    patternsOfFourTwo[0] += dp[i - 4];
-                    patternsOfFourTwo[2] += dp[i - 4];
-                    patternsOfFourTwo[5] += dp[i - 4];
-
-                    four = patternsOfFourTwo[1];
-                    two = patternsOfFourTwo[0];
-                } else if (i % 3 == 2) {
-                    patternsOfFourTwo[1] += dp[i - 4];
-                    patternsOfFourTwo[2] += dp[i - 4];
-                    patternsOfFourTwo[4] += dp[i - 4];
-
-                    four = patternsOfFourTwo[3];
-                    two = patternsOfFourTwo[2];
-                } else {
-                    patternsOfFourTwo[0] += dp[i - 4];
-                    patternsOfFourTwo[3] += dp[i - 4];
-                    patternsOfFourTwo[4] += dp[i - 4];
-
-                    four = patternsOfFourTwo[5];
-                    two = patternsOfFourTwo[4];
+        for(int i = 3; i <=n; i++){
+            long sum = dp[i-1] + dp[i-2] * 2 + dp[i-3] * 5;
+            int turn = 0;
+            for(int j = i-4; j >= 0; j--){
+                if(turn == 2){
+                    sum += dp[j] * 4;
+                    turn = 0;
+                }else{
+                    sum += dp[j] * 2;
+                    turn++;
                 }
-                
             }
-            
-            dp[i] += four * 4 + two * 2;
-            dp[i] %= 1_000_000_007;
+            dp[i] = sum % per;
         }
-        
-        return (int) dp[n];
+        return (int)(dp[n] % per);
     }
 }
