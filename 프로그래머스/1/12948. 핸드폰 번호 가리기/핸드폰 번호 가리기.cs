cefe8d396a10d92ec0af6fa5574b2ca5a7037434
SHIN HYUNJU <71755508+Shyunju@users.noreply.github.com>
2024-11-20 19:18:36 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.20 ms, Memory: 30.9 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.cs
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.cs
@@ -1,10 +1,7 @@
 public class Solution {
     public string solution(string phone_number) {
-        string number = phone_number.Substring(phone_number.Length - 4);
-        string star = "";
-        for(int i = 0; i < phone_number.Length-4; i++){
-            star += "*";
-        }
-        return star + number;
+        string answer = phone_number.Substring(phone_number.Length -4);
+        answer = answer.PadLeft(phone_number.Length, '*');
+        return answer;
     }
 }
