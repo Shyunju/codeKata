748ea6d1bbda750aacc61a9147fe0302db2fb7c1
SHIN HYUNJU <71755508+Shyunju@users.noreply.github.com>
2024-11-25 17:37:45 +0900

    [level 1] Title: 과일 장수, Time: 232.90 ms, Memory: 49.8 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.cs
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.cs
@@ -2,11 +2,10 @@ using System;
 using System.Collections.Generic;
 public class Solution {
     public int solution(int k, int m, int[] score) {
-        int all = score.Length;
         Array.Sort(score);
         Array.Reverse(score);
         int answer = 0;
-        for(int i = m-1; i < all; i += m){
+        for(int i = m-1; i < score.Length; i += m){
             answer += score[i] * m;
         }
         return answer;
