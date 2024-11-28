db428cafd3ec7e63706c24cc0a19e5a841dac563
SHIN HYUNJU <71755508+Shyunju@users.noreply.github.com>
2024-11-28 21:50:51 +0900

    [level 1] Title: 삼총사, Time: 0.22 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/131705. 삼총사/삼총사.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class Solution {
+    public int solution(int[] number) {
+        int answer = 0;
+        for(int i = 0; i < number.Length -2 ; i++){
+            for(int j = i+1; j < number.Length - 1; j++){
+                for(int k = j+1; k < number.Length; k++){
+                    int sum = number[i] + number[j] + number[k];
+                    if(sum == 0)
+                        answer++;
+                }
+            }
+        }
+        return answer;
+    }
+}
