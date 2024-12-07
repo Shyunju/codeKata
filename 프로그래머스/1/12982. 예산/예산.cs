174dd82976932f085f79f3c060265ff3c4cd1493
SHIN HYUNJU <71755508+Shyunju@users.noreply.github.com>
2024-12-07 16:35:58 +0900

    [level 1] Title: 예산, Time: 2.53 ms, Memory: 31 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.cs
+++ 프로그래머스/1/12982. 예산/예산.cs
@@ -3,14 +3,12 @@ using System;
 public class Solution {
     public int solution(int[] d, int budget) {
         int answer = 0;
-        long sum = 0;
         Array.Sort(d);
-        for(int i = 0; i < d.Length; i++)
-        {
-            sum += d[i];
-            if(sum > budget)
+        for(int i = 0; i < d.Length; i++){
+            if(budget < d[i])
                 break;
             answer++;
+            budget -= d[i];
         }
         return answer;
     }
