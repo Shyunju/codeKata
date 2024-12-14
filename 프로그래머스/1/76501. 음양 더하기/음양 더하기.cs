b55c9241dcfbe5887069f5dd9d78ed7c6f264d99
SHIN HYUNJU <71755508+Shyunju@users.noreply.github.com>
2024-12-14 17:12:55 +0900

    [level 1] Title: 음양 더하기, Time: 0.28 ms, Memory: 31.5 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.cs
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.cs
@@ -4,9 +4,7 @@ public class Solution {
     public int solution(int[] absolutes, bool[] signs) {
         int sum = 0;
         for(int i = 0; i < signs.Length; i++){
-            if(!signs[i])
-                absolutes[i] *= -1;
-            sum += absolutes[i];
+            sum += signs[i] ? absolutes[i] : -absolutes[i];
         }
         return sum;
     }
