46f824a952f4c4b9ed092ae1d418fbb30f5d34d3
SHIN HYUNJU <71755508+Shyunju@users.noreply.github.com>
2024-12-22 22:46:33 +0900

    [level 1] Title: 달리기 경주, Time: 434.00 ms, Memory: 186 MB -BaekjoonHub

--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.cs
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.cs
@@ -2,21 +2,19 @@ using System;
 using System.Collections.Generic;
 public class Solution {
     public string[] solution(string[] players, string[] callings) {
-        Dictionary<string, int> dic = new Dictionary<string, int>();
-        for(int i = 0; i < players.Length; i++)
-        {
-            dic.Add(players[i], i);            
+        var dic = new Dictionary<string, int>();
+        for(int i = 0; i < players.Length; i++){
+            dic.Add(players[i],i);
         }
-        foreach(string name in callings)
-        {
+        foreach(string name in callings){
             int rank = dic[name];
-            string temp = players[rank -1];
+            string temp = players[rank-1];
             
-            players[rank -1] = name;
+            players[rank-1] = name;
             players[rank] = temp;
             
-            dic[name] -= 1;
-            dic[temp] += 1;
+            dic[name]--;
+            dic[temp]++;
         }
         return players;
     }
