463a4b9e81c9ff807398dc9564bb7e64554a0b97
SHIN HYUNJU <71755508+Shyunju@users.noreply.github.com>
2024-12-24 14:29:04 +0900

    [Bronze V] Title: 사칙연산, Time: 52 ms, Memory: 5520 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cs
@@ -0,0 +1,25 @@
+using System;
+namespace 백준
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Program p = new Program(); //전역변수 사용을 위한 인스턴스 화
+            
+            string s = Console.ReadLine();
+            string[] sl = s.Split(" "); 
+
+            int a = int.Parse(sl[0]);
+            int b = int.Parse(sl[1]);
+            
+            Console.WriteLine(a+b);
+            Console.WriteLine(a-b);
+            Console.WriteLine(a*b);
+            Console.WriteLine(a/b);
+            Console.WriteLine(a%b);
+
+        }
+    }
+}
