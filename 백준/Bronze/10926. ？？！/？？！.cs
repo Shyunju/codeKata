f137e0a1c9efa5bf8ed9e26468a3273f55ed15fb
SHIN HYUNJU <71755508+Shyunju@users.noreply.github.com>
2024-12-25 16:54:26 +0900

    [Bronze V] Title: ??!, Time: 48 ms, Memory: 5384 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.cs
@@ -0,0 +1,15 @@
+using System;
+namespace 백준
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Program p = new Program(); //전역변수 사용을 위한 인스턴스 화
+            
+            string s = Console.ReadLine();
+            Console.WriteLine(s + "??!");
+        }
+    }
+}
