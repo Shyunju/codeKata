a9fcc23f14440eb24b218d321ef01ed0a6f3a16b
SHIN HYUNJU <71755508+Shyunju@users.noreply.github.com>
2024-12-28 17:57:03 +0900

    [Bronze V] Title: 개, Time: 40 ms, Memory: 5328 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.cs
@@ -0,0 +1,12 @@
+using System;
+
+internal class Program
+{
+    static void Main(string[] args){
+        Console.WriteLine("|\\_/|");
+        Console.WriteLine("|q p|   /}");
+        Console.WriteLine("( 0 )\"\"\"\\");
+        Console.WriteLine("|\"^\"`    |");
+        Console.WriteLine("||_/=\\\\__|");
+    }
+}
