4b5682fac49f4f1375096b001ab34bd7ea2605c1
SHIN HYUNJU <71755508+Shyunju@users.noreply.github.com>
2025-01-23 16:45:32 +0900

    [Bronze V] Title: 아스키 코드, Time: 52 ms, Memory: 5464 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cs
@@ -0,0 +1,12 @@
+using System;
+
+internal class Program
+{
+    static void Main(string[] args){
+        string s = Console.ReadLine();
+        char c = Convert.ToChar(s);
+        int answer = Convert.ToInt32(c);
+        Console.Write(answer);
+    
+    }
+}
