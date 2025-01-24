5c24beadbd6308345202198e27b15e9d027a72ef
SHIN HYUNJU <71755508+Shyunju@users.noreply.github.com>
2025-01-24 23:21:20 +0900

    [Bronze IV] Title: 숫자의 합, Time: 48 ms, Memory: 5576 KB -BaekjoonHub

---
+++ 백준/Bronze/11720. 숫자의 합/숫자의 합.cs
@@ -0,0 +1,14 @@
+using System;
+
+internal class Program
+{
+    static void Main(string[] args){
+        int n = int.Parse(Console.ReadLine());
+        string s = Console.ReadLine();
+        int answer = 0;
+        for(int i = 0; i < n; i++){
+             answer += int.Parse(s[i].ToString());
+        }
+        Console.Write(answer);
+    }
+}
