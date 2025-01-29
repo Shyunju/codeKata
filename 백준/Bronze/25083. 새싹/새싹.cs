7b7658ab3719a5c6cbe05fc961fbb8543542fa54
SHIN HYUNJU <71755508+Shyunju@users.noreply.github.com>
2025-01-29 17:02:41 +0900

    [Bronze V] Title: 새싹, Time: 44 ms, Memory: 5328 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cs
@@ -0,0 +1,8 @@
+using System;
+
+internal class Program
+{
+    static void Main(string[] args){
+        Console.Write("         ,r'\"7\nr`-_   ,'  ,/\n \\. \". L_r'\n   `~\\/\n      |\n      |");
+    }
+}
