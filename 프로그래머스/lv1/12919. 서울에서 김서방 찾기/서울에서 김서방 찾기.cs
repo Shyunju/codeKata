67c4a1c121b2a3315fcc5147ce2fbbb34a2d2ea6
SHIN HYUNJU <71755508+Shyunju@users.noreply.github.com>
2025-02-07 23:17:42 +0900

    서울에서 김서방 찾기.cs 업데이트

--- 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.cs
+++ 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.cs
@@ -1,12 +1,12 @@
 public class Solution {
     public string solution(string[] seoul) {
-        string answer = "";
+        //string answer = "";
         int kim = 0;
         for(int i =0; i < seoul.Length ; i++){
             if(seoul[i] == "Kim"){
                 kim = i;
             }
         }
-        return answer = "김서방은 "+ kim.ToString() + "에 있다";
+        return string answer = "김서방은 "+ kim.ToString() + "에 있다";
     }
 }
