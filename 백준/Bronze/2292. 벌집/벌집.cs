1c5f0ea23ecd8c8527b15bbca9f699ebf2178b42
SHIN HYUNJU <71755508+Shyunju@users.noreply.github.com>
2025-02-24 14:11:00 +0900

    [Bronze II] Title: 벌집, Time: 52 ms, Memory: 5532 KB -BaekjoonHub

---
+++ 백준/Bronze/2292. 벌집/벌집.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+
+internal class Program
+{
+    static void Main(string[] args){
+        double n = double.Parse(Console.ReadLine());
+        
+        if(n == 1)
+            Console.Write(1);
+        else{
+            int start = 0;
+            int gap = 1;
+            int answer = 1;
+            
+            while(true){
+                if(n >= (6* start + 2) && n <= (6 * (start + gap) +1)){
+                    Console.Write(answer + 1);
+                    return;
+                }
+                start += gap;
+                gap++;
+                answer++;
+            }
+        }
+    }
+}
