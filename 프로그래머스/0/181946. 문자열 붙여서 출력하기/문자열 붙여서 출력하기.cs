bc51ab9a7ba4c5441f74eb39ecdd3c7b7cbe08f2
SHIN HYUNJU <71755508+Shyunju@users.noreply.github.com>
2025-03-04 13:50:17 +0900

    [level 0] Title: 문자열 붙여서 출력하기, Time: 25.09 ms, Memory: 16.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181946. 문자열 붙여서 출력하기/문자열 붙여서 출력하기.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class Example
+{
+    public static void Main()
+    {
+        String[] input;
+
+        Console.Clear();
+        input = Console.ReadLine().Split(' ');
+
+        String s1 = input[0];
+        String s2 = input[1];
+        Console.Write(s1 + s2);
+
+    }
+}
