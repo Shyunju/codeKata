6d44e4a4d5a3fd92211c6c63238d7692863881d8
SHIN HYUNJU <71755508+Shyunju@users.noreply.github.com>
2025-03-08 18:14:05 +0900

    약수의 합.cs 업데이트

--- 프로그래머스/1/12928. 약수의 합/약수의 합.cs
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.cs
@@ -5,7 +5,7 @@ public class Solution {
         for(int i = 1; i <= n /2; i++){
             if(n % i == 0){
                 answer += i;
-                //answer += n / i;
+                
             }
         }
         return answer;
