72c060b81d1563b1a453ed78e4bda88a0eccb5d5
SHIN HYUNJU <71755508+Shyunju@users.noreply.github.com>
2025-03-24 15:42:38 +0900

    [level 1] Title: 약수의 합, Time: 0.27 ms, Memory: 31.3 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.cs
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.cs
@@ -1,12 +1,10 @@
 public class Solution {
     public int solution(int n) {
         int answer = n;
-        
-        for(int i = 1; i <= n /2; i++){
-            if(n % i == 0){
+        for(int i = 1; i <= n / 2; ++i)
+        {
+            if(n % i == 0)
                 answer += i;
-                
-            }
         }
         return answer;
     }
