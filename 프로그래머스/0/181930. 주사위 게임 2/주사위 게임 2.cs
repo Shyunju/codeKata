59402a500322a6c3a3b806c1c9dc6efb7ae2c3bc
SHIN HYUNJU <71755508+Shyunju@users.noreply.github.com>
2025-04-21 16:08:43 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.27 ms, Memory: 31.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Solution {
+    public int solution(int a, int b, int c) {
+        if(a == b && b == c){
+            return(a +b +c) * (a*a + b*b + c*c) * ( a*a*a + b*b*b + c*c*c);
+        }else if(a ==b || b == c || a == c){
+            return(a+b+c) * (a*a + b*b + c*c);
+        }else{
+            return a+b+c;
+        }
+    }
+}
