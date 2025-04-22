a0c5f7911369d3b937e43f6c4cef20727fa671de
SHIN HYUNJU <71755508+Shyunju@users.noreply.github.com>
2025-04-22 12:10:32 +0900

    [level 0] Title: 문자열 돌리기, Time: 16.84 ms, Memory: 15.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181945. 문자열 돌리기/문자열 돌리기.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class Example
+{
+    public static void Main()
+    {
+        String s;
+
+        Console.Clear();
+        s = Console.ReadLine();
+        foreach(var i in s){
+            Console.WriteLine(i);
+        }
+
+    }
+}
