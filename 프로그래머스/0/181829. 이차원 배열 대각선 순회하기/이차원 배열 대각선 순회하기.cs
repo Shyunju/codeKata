7ec531ed09ede904d0acc7f180112a0c5dd2b85b
SHIN HYUNJU <71755508+Shyunju@users.noreply.github.com>
2025-05-01 16:56:08 +0900

    [level 0] Title: 이차원 배열 대각선 순회하기, Time: 0.35 ms, Memory: 31.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181829. 이차원 배열 대각선 순회하기/이차원 배열 대각선 순회하기.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Solution {
+    public int solution(int[,] board, int k) {
+        int answer = 0;
+        for(int i = 0; i < board.GetLength(0); i++){
+            for(int j = 0; j < board.GetLength(1); j++){
+                if(i + j <= k)
+                    answer += board[i,j];
+            }
+        }
+        return answer;
+    }
+}
