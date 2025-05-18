e3f9ccf5ef6bbc10a367100c061c629b87855173
SHIN HYUNJU <71755508+Shyunju@users.noreply.github.com>
2025-05-18 23:20:08 +0900

    [level 0] Title: 세 개의 구분자, Time: 20.26 ms, Memory: 49.3 MB -BaekjoonHub

--- 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.cs
+++ 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.cs
@@ -1,34 +1,9 @@
 using System;
-using System.Collections.Generic;
+
 public class Solution {
     public string[] solution(string myStr) {
-        string[] arr1 = myStr.Split('a');
-        var list = new List<string>();
-        foreach(string i in arr1)
-        {
-            string[] arr2 = i.Split('b');
-            for(int j = 0; j< arr2.Length; j++)
-            {
-                if(arr2[j].Length == 0)
-                    continue;
-                list.Add(arr2[j]);
-            }
-        }
-        var answer = new List<string>();
-        foreach(string i in list)
-        {
-            string[] arr3 = i.Split('c');
-            for(int j = 0; j < arr3.Length; j++)
-            {
-                if(arr3[j].Length == 0)
-                    continue;
-                answer.Add(arr3[j]);
-            }
-        }
-        string[] empty = {"EMPTY"};
-        if(answer.Count == 0)
-            return empty;
-        return answer.ToArray();
-        
+        char[] separators = new char[3]{ 'a', 'b', 'c' };
+        string[] answer = myStr.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+        return answer.Length == 0 ? new string[1]{ "EMPTY" } : answer; 
     }
 }
