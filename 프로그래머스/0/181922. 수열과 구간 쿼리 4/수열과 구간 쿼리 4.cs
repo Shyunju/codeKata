9a8782ce5d32fd4d353528816be629a964398ff2
SHIN HYUNJU <71755508+Shyunju@users.noreply.github.com>
2025-05-21 16:56:24 +0900

    [level 0] Title: 수열과 구간 쿼리 4, Time: 0.74 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.cs
@@ -0,0 +1,21 @@
+using System;
+
+public class Solution {
+    public int[] solution(int[] arr, int[,] queries) {
+        for(int i = 0; i < queries.GetLength(0); i++)
+        {
+            int s = queries[i,0];
+            int e = queries[i,1];
+            int k = queries[i,2];
+            
+            for(int j = s; j <= e; j++)
+            {
+                if( j % k == 0)
+                {
+                    arr[j]++;
+                }
+            }
+        }
+        return arr;
+    }
+}
