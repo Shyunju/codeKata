4fe591820ff1556d0e5892fcaefab85e99f681c9
SHIN HYUNJU <71755508+Shyunju@users.noreply.github.com>
2025-06-13 15:38:25 +0900

    [level 0] Title: 문자열 출력하기, Time: 18.48 ms, Memory: 16.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181952. 문자열 출력하기/문자열 출력하기.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Example
+{
+    public static void Main()
+    {
+        String s;
+
+        Console.Clear();
+        s = Console.ReadLine();
+        Console.WriteLine(s);
+
+    }
+}
