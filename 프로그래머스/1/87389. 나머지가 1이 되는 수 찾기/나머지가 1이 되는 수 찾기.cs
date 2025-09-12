bceae04eca46d20e6d4fadd8bc29a8c16c842e05
SHIN HYUNJU <71755508+Shyunju@users.noreply.github.com>
2025-09-12 16:52:24 +0900

    [level 1] Title: 나머지가 1이 되는 수 찾기, Time: 3.02 ms, Memory: 31.4 MB -BaekjoonHub

--- 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.cs
+++ 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.cs
@@ -2,14 +2,11 @@ using System;
 
 public class Solution {
     public int solution(int n) {
-        int answer = 2;
-        while(true){
-            if(n % answer != 1){
-                answer++;
-                continue;
-            }
-            break;
+        for(int x = 2; x < n; x++)
+        {
+            if(n % x == 1)
+                return x;
         }
-        return answer;
+        return n-1;
     }
 }
