ffcf82dee0c1a218f507ecf45557e7e409042f38
SHIN HYUNJU <71755508+Shyunju@users.noreply.github.com>
2025-09-13 13:57:44 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.38 ms, Memory: 31.1 MB -BaekjoonHub

--- 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.cs
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.cs
@@ -1,6 +1,8 @@
 public class Solution {
     public string solution(string s) {
-        string answer = s.Length % 2 == 0 ? s.Substring(s.Length/2 -1, 2) : s[s.Length/2].ToString();
-        return answer;
+        if(s.Length % 2 != 0)
+            return s[s.Length/2].ToString();
+        
+        return s[s.Length/2 -1].ToString() + s[s.Length/2].ToString();
     }
 }
