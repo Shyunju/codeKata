962e618f206450de8fede77bb4afa9baab1bfe05
SHIN HYUNJU <71755508+Shyunju@users.noreply.github.com>
2025-10-13 00:32:50 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 0.21 ms, Memory: 31.5 MB -BaekjoonHub

--- 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.cs
+++ 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.cs
@@ -2,14 +2,6 @@ using System;
 public class Solution {
     public long solution(int a, int b) {
         long answer = 0;
-        if( a == b){
-            return answer = a;
-        }
-        int min = Math.Min(a,b);
-        int max = Math.Max(a,b);
-        for ( int i = min; i <= max ; i++ ){
-            answer += i;
-        }
-        return answer;
+        return answer = ((long)Math.Abs(b - a) + 1) * (a + b) / 2;
     }
 }
