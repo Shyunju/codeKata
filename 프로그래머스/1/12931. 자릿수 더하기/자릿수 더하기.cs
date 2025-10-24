cafcd2e78f791e89b6f10298fc24417912ffcefb
SHIN HYUNJU <71755508+Shyunju@users.noreply.github.com>
2025-10-24 15:54:07 +0900

    [level 1] Title: 자릿수 더하기, Time: 1.33 ms, Memory: 31.4 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.cs
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.cs
@@ -3,9 +3,10 @@ using System;
 public class Solution {
     public int solution(int n) {
         int answer = 0;
-        while(n > 0){
-            answer += n % 10;
-            n /= 10;
+        string num = n.ToString();
+        foreach(char i in num)
+        {
+            answer += int.Parse(i.ToString());
         }
         return answer;
     }
