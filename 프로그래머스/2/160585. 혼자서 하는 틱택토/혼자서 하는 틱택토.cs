1e45b952060a70788b9597c8ed2a3d72ce7e37c3
SHIN HYUNJU <71755508+Shyunju@users.noreply.github.com>
2025-10-30 20:09:40 +0900

    [level 2] Title: 혼자서 하는 틱택토, Time: 0.61 ms, Memory: 31 MB -BaekjoonHub

--- 프로그래머스/2/160585. 혼자서 하는 틱택토/혼자서 하는 틱택토.cs
+++ 프로그래머스/2/160585. 혼자서 하는 틱택토/혼자서 하는 틱택토.cs
@@ -1,46 +1,54 @@
 using System;
 
 public class Solution {
-    public int solution(string[] board) {
-        int cnt_o = 0; 
-        int cnt_x = 0;
-        for(int i = 0; i < 3; i++){
-            for(int j = 0; j < 3; j++){
-                if(board[i][j] == 'O') ++cnt_o;
-                else if(board[i][j] == 'X') ++cnt_x;
+    public int solution(string[] board) 
+    {
+        int count_O = 0;
+        int count_X = 0;
+        for(int y = 0; y < 3; ++y)
+        {
+            for(int x = 0; x < 3; ++x)
+            {
+                if(board[y][x] == 'O') ++count_O;
+                else if(board[y][x] == 'X') ++count_X;
             }
         }
-        
-        if(cnt_o > cnt_x + 1) return 0;
-        if(cnt_x > cnt_o) return 0;
-        
-        int bingo_o = 0;
-        int bingo_x = 0;
-        
-        //가로선
-        for(int y = 0; y < 3; y++){
-            if(board[y] == "OOO") ++bingo_o;
-            else if(board[y] == "XXX") ++bingo_x;
+        if(count_O > count_X + 1) return 0;
+        if(count_X > count_O) return 0;
+
+        int bingo_O = 0;
+        int bingo_X = 0;
+
+        for(int y = 0; y < 3; ++y) // 가로
+        {
+            if(board[y] == "OOO") ++bingo_O;
+            else if(board[y] == "XXX") ++bingo_X;
         }
-        //세로선\
-        for(int x = 0; x < 3; x++){
-            if(board[0][x] == 'O' && board[1][x] == 'O' && board[2][x] == 'O') ++bingo_o;
-            else if(board[0][x] == 'X' && board[1][x] == 'X' && board[2][x] == 'X') ++bingo_x;
+
+        for(int y = 0; y < 3; ++y) // 세로
+        {
+            if(board[0][y] == 'O' && board[1][y] == 'O' && board[2][y] == 'O') ++bingo_O;
+            else if(board[0][y] == 'X' && board[1][y] == 'X' && board[2][y] == 'X') ++bingo_X;
         }
-        //대각선
-        if(board[1][1] == 'O'){
-            if(board[0][0] == 'O' && board[2][2] == 'O') ++bingo_o;
-            if(board[0][2] == 'O' && board[2][0] == 'O') ++bingo_o;
+
+        if(board[1][1] == 'O')
+        {
+            if((board[0][0] == 'O' && board[2][2] == 'O')) ++bingo_O;
+            if((board[0][2] == 'O' && board[2][0] == 'O')) ++bingo_O;
         }
-        else if(board[1][1] == 'X'){
-            if(board[0][0] == 'X' && board[2][2] == 'X') ++bingo_x;
-            if(board[0][2] == 'X' && board[2][0] == 'X') ++bingo_x;
+        else if(board[1][1] == 'X')
+        {
+            if((board[0][0] == 'X' && board[2][2] == 'X')) ++bingo_X;
+            if((board[0][2] == 'X' && board[2][0] == 'X')) ++bingo_X;
         }
-        
-        if(bingo_o > 0 && bingo_x > 0) return 0;
-        if(bingo_o > 0 && cnt_o == cnt_x) return 0;
-        if(bingo_x > 0 && cnt_o > cnt_x) return 0;
-        
+
+        if(bingo_O > 0 && bingo_X > 0) return 0;
+
+        if(bingo_O > bingo_X && count_O == count_X) return 0;
+
+        if(bingo_X > bingo_O && count_O > count_X) return 0;
+
         return 1;
     }
+
 }
