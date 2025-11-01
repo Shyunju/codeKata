a7ec9baecbdeff9ed422689eecd0a20ee63104e8
SHIN HYUNJU <71755508+Shyunju@users.noreply.github.com>
2025-11-01 17:00:41 +0900

    [level 1] Title: 콜라 문제, Time: 0.26 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Solution {
+    public int solution(int a, int b, int n) {
+        int answer = 0;
+        while(n >= a)
+        {
+            answer += n / a * b;
+            n = n / a * b + n % a;
+        }
+        return answer;
+    }
+}
