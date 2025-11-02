5a274aeb9bce25e78ee19045a177350eda4add85
SHIN HYUNJU <71755508+Shyunju@users.noreply.github.com>
2025-11-02 21:28:35 +0900

    [level 2] Title: n^2 배열 자르기, Time: 3.58 ms, Memory: 37.3 MB -BaekjoonHub

--- 프로그래머스/2/87390. n＾2 배열 자르기/README.md
+++ 프로그래머스/2/87390. n＾2 배열 자르기/README.md
@@ -1,10 +1,10 @@
 # [level 2] n^2 배열 자르기 - 87390 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87390) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87390#qna) 
 
 ### 성능 요약
 
-메모리: 37.6 MB, 시간: 3.08 ms
+메모리: 37.3 MB, 시간: 3.58 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 18일 14:48:35
+2025년 11월 02일 21:28:34
 
 ### 문제 설명
 
--- 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.cs
+++ 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.cs
@@ -3,8 +3,14 @@ using System.Collections.Generic;
 public class Solution {
     public int[] solution(int n, long left, long right) {
         List<int> answer = new List<int>();
-        for(long i = left; i <= right; i++){
-            answer.Add((int)Math.Max(i / n , i % n) + 1);
+        
+        long idx = left;
+        while(idx <= right)
+        {
+            int y = (int)(idx / n) + 1;
+            int x = (int)(idx % n) + 1;
+            answer.Add(Math.Max(y,x));
+            idx++;
         }
         return answer.ToArray();
     }
