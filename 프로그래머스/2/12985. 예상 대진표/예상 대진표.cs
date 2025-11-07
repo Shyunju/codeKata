124973831c70c1fba19bb40a3889279350887eca
SHIN HYUNJU <71755508+Shyunju@users.noreply.github.com>
2025-11-07 16:16:28 +0900

    [level 2] Title: 예상 대진표, Time: 0.32 ms, Memory: 31.3 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.cs
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.cs
@@ -1,17 +1,19 @@
 using System;
+
 class Solution
 {
     public int solution(int n, int a, int b)
     {
-        int answer = 0;
+        int answer = 1;
+        int A = Math.Min(a,b);
+        int B = Math.Max(a,b);
         while(true)
         {
-            answer++;
-            a = (a+1)/2;
-            b = (b+1)/2;
-            
-            if( a == b)
+            if(B-A == 1 && B % 2 == 0)
                 break;
+            A = A % 2 == 0 ? A / 2 : A / 2 + 1;
+            B = B % 2 == 0 ? B / 2 : B / 2 + 1;
+            answer++;
         }
 
         return answer;
