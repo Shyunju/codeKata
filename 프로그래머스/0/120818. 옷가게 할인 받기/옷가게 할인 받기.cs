643de11423311a324bf4d6b0769b2d5da160ae81
SHIN HYUNJU <71755508+Shyunju@users.noreply.github.com>
2025-11-23 20:12:32 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.17 ms, Memory: 31.3 MB -BaekjoonHub

--- 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.cs
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.cs
@@ -2,15 +2,13 @@ using System;
 
 public class Solution {
     public int solution(int price) {
-        int answer = 0;
+        float sale = 1f;
         if(price >= 500000)
-            answer = (int)(price * 0.8f);
-        else if(price >=300000)
-            answer = (int)(price * 0.9f);
+            sale = 0.8f;
+        else if(price >= 300000)
+            sale = 0.9f;
         else if(price >= 100000)
-            answer = (int)(price * 0.95f);
-        else
-            answer = price;
-        return answer;
+            sale = 0.95f;
+        return (int)(price * sale);
     }
 }
