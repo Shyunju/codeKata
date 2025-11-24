41518c3f4dc668c53b67ea7015daa915f28e7155
SHIN HYUNJU <71755508+Shyunju@users.noreply.github.com>
2025-11-24 14:34:38 +0900

    [Gold II] Title: 피보나치 수 3, Time: 48 ms, Memory: 5504 KB -BaekjoonHub

---
+++ 백준/Gold/2749. 피보나치 수 3/피보나치 수 3.cs
@@ -0,0 +1,39 @@
+using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        long n = long.Parse(Console.ReadLine());
+
+        const int MOD = 1000000;
+        const int PISANO = 1500000;
+
+        // 피사노 주기를 이용해 n을 줄임
+        int reducedN = (int)(n % PISANO);
+
+        if (reducedN == 0)
+        {
+            Console.WriteLine(0);
+            return;
+        }
+        if (reducedN == 1 || reducedN == 2)
+        {
+            Console.WriteLine(1);
+            return;
+        }
+
+        int a = 1; // F(1)
+        int b = 1; // F(2)
+        int c = 0;
+
+        for (int i = 3; i <= reducedN; i++)
+        {
+            c = (a + b) % MOD;
+            a = b;
+            b = c;
+        }
+
+        Console.WriteLine(b);
+    }
+}
