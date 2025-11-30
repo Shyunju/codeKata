e27fba999540055a8a1f3b85145d7cc7ca5360ce
SHIN HYUNJU <71755508+Shyunju@users.noreply.github.com>
2025-11-30 19:35:17 +0900

    [level 0] Title: 양꼬치, Time: 0.16 ms, Memory: 31.5 MB -BaekjoonHub

--- 프로그래머스/0/120830. 양꼬치/양꼬치.cs
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.cs
@@ -2,7 +2,6 @@ using System;
 
 public class Solution {
     public int solution(int n, int k) {
-        int answer = 12000 * n + 2000 * (k - n / 10);
-        return answer;
+        return 12000 * n + 2000 * (k - n / 10);
     }
 }
