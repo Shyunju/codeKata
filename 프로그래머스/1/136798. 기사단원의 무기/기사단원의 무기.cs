367836559c2349f3a3994b3c5ca8d0c0471a57c6
SHIN HYUNJU <71755508+Shyunju@users.noreply.github.com>
2025-12-01 20:10:56 +0900

    [level 1] Title: 기사단원의 무기, Time: 5.01 ms, Memory: 31.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.cs
@@ -0,0 +1,23 @@
+using System;
+
+public class Solution {
+    public int solution(int number, int limit, int power) {
+        int answer = 0;
+        int[] count = new int[number+1];
+        for(int i=1;i<=number;i++)
+        {
+            for(int j = i; j <= number; j += i)
+            {
+                count[j]++;
+            }
+        }
+        for(int i = 1; i <= number; i++)
+        {
+            if(count[i] > limit) 
+                count[i] = power;
+            answer += count[i];
+        }
+
+        return answer;
+    }
+}
