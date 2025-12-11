a6fb922596fdc33bce68a5425c9963016d8aea31
SHIN HYUNJU <71755508+Shyunju@users.noreply.github.com>
2025-12-11 17:51:49 +0900

    [level 3] Title: 네트워크, Time: 0.29 ms, Memory: 31.2 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.cs
+++ 프로그래머스/3/43162. 네트워크/네트워크.cs
@@ -4,21 +4,23 @@ public class Solution {
     public int solution(int n, int[,] computers) {
         int answer = 0;
         bool[] visited = new bool[n];
-        
-        for(int i = 0; i < n; i++){
-            if(!visited[i]){
+        for(int i = 0; i < n; i++)
+        {
+            if(!visited[i])
+            {
                 answer++;
                 DFS(computers, visited, i);
             }
         }
         return answer;
     }
-    private void DFS(int[,] computers, bool[] visited, int cur){
-        visited[cur] = true;
-        for(int i = 0; i < computers.GetLength(0); i++){
-            if(computers[cur, i] == 1 && !visited[i]){
+    void DFS(int[,] computers, bool[] visited, int start)
+    {
+        visited[start] = true;
+        for(int i = 0; i < computers.GetLength(0); i++)
+        {
+            if(!visited[i] && computers[start, i] == 1)
                 DFS(computers, visited, i);
-            }
         }
     }
 }
