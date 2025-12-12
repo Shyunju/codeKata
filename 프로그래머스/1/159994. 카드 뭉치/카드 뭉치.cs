b4098de04fcfd395a5440a45a7c46a89901ba0b7
SHIN HYUNJU <71755508+Shyunju@users.noreply.github.com>
2025-12-12 23:18:43 +0900

    [level 1] Title: 카드 뭉치, Time: 0.23 ms, Memory: 30.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.cs
@@ -0,0 +1,21 @@
+using System;
+
+public class Solution {
+    public string solution(string[] cards1, string[] cards2, string[] goal) {
+        string answer = "Yes";
+        if ( cards1.Length + cards2.Length < goal.Length)
+            return "No";
+        int first = 0;
+        int second = 0;
+        for(int i = 0; i< goal.Length; i++){
+            if(first < cards1.Length && goal[i] == cards1[first]){
+                first++;
+            }else if(second < cards2.Length && goal[i] == cards2[second]){
+                second++;
+            }else{
+                return "No";
+            }
+        }
+        return answer;
+    }
+}
