5f3c41b6daf74623136b5288930363be26051feb
SHIN HYUNJU <71755508+Shyunju@users.noreply.github.com>
2025-12-26 14:48:00 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 1.82 ms, Memory: 31.1 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.cs
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.cs
@@ -1,15 +1,10 @@
 using System;
 using System.Linq;
-using System.Collections.Generic;
 public class Solution {
     public string solution(string s) {
-        string[] numS = s.Split(" ");
-        var num = new List<int>();
-        foreach(string i in numS){
-            int n = int.Parse(i);
-            num.Add(n);
-        }
-        string answer = num.Min() + " " + num.Max();
-        return answer;
+        int[] num = Array.ConvertAll(s.Split(), int.Parse);
+        int min = num.Min();
+        int max = num.Max();
+        return min + " " + max;
     }
 }
