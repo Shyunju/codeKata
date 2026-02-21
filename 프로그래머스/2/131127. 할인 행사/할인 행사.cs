7a25913a7a491287eea17a49223eb7141250777e
SHIN HYUNJU <71755508+Shyunju@users.noreply.github.com>
2026-02-21 16:42:35 +0900

    [level 2] Title: 할인 행사, Time: 1682.75 ms, Memory: 50.4 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.cs
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.cs
@@ -1,72 +1,26 @@
 using System;
 using System.Collections.Generic;
-
-public class Solution
-{
-    Dictionary<string, int> dic = new Dictionary<string, int>();
-    public int solution(string[] want, int[] number, string[] discount)
-    {
+using System.Linq;
+public class Solution {
+    public int solution(string[] want, int[] number, string[] discount) {
         int answer = 0;
-        InsertData(want, number);
-				
-        answer=FirstCheck(dic,discount);
+        List<string> list = new List<string>();
+        for(int i = 0; i < want.Length; i++)
+        {
+            for(int j = 0; j < number[i]; j++)
+            {
+                list.Add(want[i]);
+            }
+        }
+        list.Sort();
         
-        for (int i = 1; i < discount.Length-9; i++)
+        for(int i = 0; i <= discount.Length - 10; i++)
         {
-
-            if (dic.ContainsKey(discount[i - 1]))
-            {
-                dic[discount[i - 1]]++;
-            }
-            if (dic.ContainsKey(discount[i + 9]))
-            {
-                dic[discount[i + 9]]--;
-            }
-            if (CheckNumber(dic))
-            {
+            List<string> result = discount.Skip(i).Take(10).ToList();
+            result.Sort();
+            if(list.SequenceEqual(result))
                 answer++;
-            }
         }
         return answer;
-    }
-
-    void InsertData(string[] want, int[] number)
-    {
-        for (int i = 0; i < want.Length; i++)
-        {
-            dic[want[i]] = number[i];
-        }
-    }
-
-    int FirstCheck(Dictionary<string, int> dic,string[] discount)
-    {
-        int answer = 0;
-        for (int i = 0; i < 10; i++)
-        {
-            if (dic.ContainsKey(discount[i]))
-            {
-                dic[discount[i]]--;
-            }
-        }
-        if (CheckNumber(dic))
-        {
-            answer++;
-        }
-        return answer;
-    }
-    
-    bool CheckNumber(Dictionary<string, int> _dic)
-    {
-        bool b = false;
-        foreach (int i in _dic.Values)
-        {
-            if (i > 0)
-            {
-                b = false;
-                break;
-            }
-            else { b = true; }
-        }
-        return b;
     }
 }
