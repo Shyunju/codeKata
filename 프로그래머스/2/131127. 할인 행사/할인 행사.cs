b2de6614971118bd982722067c376b0ecda76a6b
SHIN HYUNJU <71755508+Shyunju@users.noreply.github.com>
2026-02-24 00:51:16 +0900

    [level 2] Title: 할인 행사, Time: 35.02 ms, Memory: 49.2 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.cs
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.cs
@@ -2,25 +2,48 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 public class Solution {
-    public int solution(string[] want, int[] number, string[] discount) {
+    Dictionary<string, int> wantDict;
+    Dictionary<string, int> currentDict;
+    string[] discount;
+    public int solution(string[] want, int[] number, string[] discount) 
+    {
         int answer = 0;
-        List<string> list = new List<string>();
-        for(int i = 0; i < want.Length; i++)
+        wantDict = new Dictionary<string, int>();
+        currentDict = new Dictionary<string, int>();
+        this.discount = discount;
+        for (int i = 0; i < want.Length; i++) 
         {
-            for(int j = 0; j < number[i]; j++)
-            {
-                list.Add(want[i]);
-            }
+            wantDict[want[i]] = number[i];
         }
-        list.Sort();
+        for (int i = 0; i < 10; i++) 
+        {
+            currentDict[discount[i]] = currentDict.GetValueOrDefault(discount[i], 0) + 1;
+        }
         
-        for(int i = 0; i <= discount.Length - 10; i++)
+
+        if (IsMatch()) answer++;
+
+        for (int i = 1; i <= discount.Length - 10; i++) 
         {
-            List<string> result = discount.Skip(i).Take(10).ToList();
-            result.Sort();
-            if(list.SequenceEqual(result))
-                answer++;
+            // 나가는 원소 (i-1번째)
+            string outItem = discount[i - 1];
+            currentDict[outItem]--;
+            
+            // 들어오는 원소 (i+9번째)
+            string inItem = discount[i + 9];
+            currentDict[inItem] = currentDict.GetValueOrDefault(inItem, 0) + 1;
+
+            if (IsMatch()) answer++;
         }
+
         return answer;
+    }
+    public bool IsMatch() 
+    {
+        foreach (var pair in wantDict) 
+        {
+            if (currentDict.GetValueOrDefault(pair.Key, 0) != pair.Value) return false;
+        }
+        return true;
     }
 }
