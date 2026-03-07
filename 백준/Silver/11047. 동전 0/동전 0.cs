8ba86e90814626b28dfd8769877c9b038fae0436
SHIN HYUNJU <71755508+Shyunju@users.noreply.github.com>
2026-03-07 16:14:01 +0900

    [Silver IV] Title: 동전 0, Time: 48 ms, Memory: 5960 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.cs
+++ 백준/Silver/11047. 동전 0/동전 0.cs
@@ -1,38 +1,33 @@
 using System;
+using System.Linq;
 using System.Collections.Generic;
-namespace 백준
-{
-    internal class Program
-    {
-        
-        static void Main(string[] args)
-        {
-            Program p = new Program(); //전역변수 사용을 위한 인스턴스 화
-            int answer = 0;
-            string s = Console.ReadLine();
-            string[] sl = s.Split(" "); 
-            
-            int n = int.Parse(sl[0]);
-            int k = int.Parse(sl[1]);
-            
-            List<int> coin = new List<int>();
 
-            for (int i = 0; i < n; i++)
-            {
-                string input = Console.ReadLine();
-                int money = int.Parse(input);
-                coin.Add(money);                
-            }
-            coin.Reverse();
-            while( k > 0){
-                if( k / coin[0] > 0){
-                    answer += k / coin[0];
-                    k %= coin[0];
-                }
-                coin.RemoveAt(0);
-            }
-            
-            Console.Write(answer);
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Program p = new Program();
+        int answer = 0;
+        string[] sl = Console.ReadLine().Split();
+        
+        int n = int.Parse(sl[0]);
+        int k = int.Parse(sl[1]);
+        
+        List<int> coin = new List<int>();
+        for(int i = 0; i < n; i++)
+        {
+            coin.Add(int.Parse(Console.ReadLine()));
         }
+        coin.Reverse();
+        while( k > 0)
+        {
+            if(k / coin[0] > 0)
+            {
+                answer += k/coin[0];
+                k %= coin[0];
+            }
+            coin.RemoveAt(0);
+        }
+        Console.WriteLine(answer);
     }
 }
