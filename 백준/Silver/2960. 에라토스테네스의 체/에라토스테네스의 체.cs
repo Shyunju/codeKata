1fc980d81904551bfdaf2207c78a15685f23b0ac
SHIN HYUNJU <71755508+Shyunju@users.noreply.github.com>
2026-03-09 16:35:17 +0900

    [Silver IV] Title: 에라토스테네스의 체, Time: 48 ms, Memory: 5720 KB -BaekjoonHub

---
+++ 백준/Silver/2960. 에라토스테네스의 체/에라토스테네스의 체.cs
@@ -0,0 +1,41 @@
+using System;
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Program p = new Program();
+        string[] input = Console.ReadLine().Split();
+        int n = int.Parse(input[0]);
+        int k = int.Parse(input[1]);
+        bool[] nums = new bool[n+1];
+        
+        nums[0] = true;
+        nums[1] = true; //true면 소수. 단, 0과 1을 제외
+        int answer = 0;
+        
+        for(int i = 2; i <= n; i++)
+        {
+            if(nums[i])    continue;
+            nums[i] = true;
+            answer++;
+            if(answer == k)
+            {
+                Console.WriteLine(i);
+                return;
+            }
+            for(int j = 2 * i; j <= n; j += i)
+            {
+                if(!nums[j])
+                {
+                    nums[j] = true;
+                    answer++;
+                    if(answer == k)
+                    {
+                        Console.WriteLine(j);
+                        return;
+                    }
+                }
+            }
+        }
+    }
+}
