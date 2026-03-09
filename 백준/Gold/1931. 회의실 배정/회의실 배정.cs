12c0d8e328865d55dc3bdda53427241f574ffc17
SHIN HYUNJU <71755508+Shyunju@users.noreply.github.com>
2026-03-09 16:08:07 +0900

    [Gold V] Title: 회의실 배정, Time: 124 ms, Memory: 19404 KB -BaekjoonHub

--- 백준/Gold/1931. 회의실 배정/회의실 배정.cs
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.cs
@@ -1,50 +1,45 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
-namespace 백준
-{
-    internal class Program
-    {
-        
-        static void Main(string[] args)
-        {
-            
-            string num = Console.ReadLine();
-            int n = int.Parse(num);
-            (int start, int end)[] time = new(int, int)[n];
-             
-            
-            for(int i = 0; i < n; i++){
-                string[] sl = Console.ReadLine().Split(" ");
-                
-                int a = int.Parse(sl[0]);
-                int b = int.Parse(sl[1]);
-                
-                time[i] = (a, b);
-            }
-            Array.Sort(time, (x, y) => Compare(x, y));
-            
-            int answer= 0;
-            int t = 0;
-            
-            for (int i = 0; i < n; i++)
-            {
-                if (t <= time[i].start) // 회의 종료 시점이 다음 회의 시작지점보다 적다면
-                {
-                    t = time[i].end;
-                    answer++;
-                }
-            }
-            Console.Write(answer);
-            
-            int Compare((int start, int end) x, (int start, int end) y)
-            {
-                if (x.end == y.end) // 끝나는 순서가 같다면
-                    return (x.start < y.start) ? -1 : 1; // 시작하는 순서가 빠른 순으로
-                else
-                    return (x.end < y.end) ? -1 : 1; // 끝나는 순서가 빠른 순으로
-            }
+using System.Collections.Generic;
 
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Program p = new Program();
+        int n = int.Parse(Console.ReadLine());
+        (int start, int end)[] time = new(int, int)[n];
+        for(int i = 0; i < n; i++)
+        {
+            string[] input = Console.ReadLine().Split();
+            int s = int.Parse(input[0]);
+            int e = int.Parse(input[1]);
+            
+            time[i] = (s, e);
+        }
+        Array.Sort(time, (x,y) => p.Compare(x,y));
+        int answer = 0;
+        int t = 0;
+        foreach(var cur in time)
+        {
+            if(t <= cur.start)
+            {
+                t = cur.end;
+                answer++;
+            }
+        }
+        Console.WriteLine(answer);
+    }
+    public int Compare((int start, int end)x, (int start, int end)y)
+    {
+        if(x.end == y.end)
+        {
+            if(x.start < y.start) return -1;
+            return 1;
+        }else
+        {
+            if(x.end < y.end) return -1;
+            return 1;
         }
     }
 }
