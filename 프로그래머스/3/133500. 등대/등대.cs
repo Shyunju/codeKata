9714bb0c4efc72b652d31274051f1303b836d115
SHIN HYUNJU <71755508+Shyunju@users.noreply.github.com>
2026-03-14 17:19:54 +0900

    Refactor loops for clarity in 등대.cs

--- 프로그래머스/3/133500. 등대/등대.cs
+++ 프로그래머스/3/133500. 등대/등대.cs
@@ -6,10 +6,12 @@ public class Solution {
         bool[] isLightOn = new bool[n+1];
         var info = new Dictionary<int, List<int>>();
         
-        for(int i = 0; i < n+1; i++){
+        for(int i = 0; i < n+1; i++)
+        {
             info.Add(i, new List<int>());
         }
-        for(int i = 0; i < lighthouse.GetLength(0); i++){
+        for(int i = 0; i < lighthouse.GetLength(0); i++)
+        {
             info[lighthouse[i,0]].Add(lighthouse[i,1]);
             info[lighthouse[i,1]].Add(lighthouse[i,0]);
         }
@@ -17,11 +19,14 @@ public class Solution {
         return answer;
     }
     private void DFS(int node, int parent, Dictionary<int, List<int>> info, bool[] isLightOn, ref int answer){
-        for(int i = 0; i < info[node].Count; i++){
-            if(info[node][i] != parent){
+        for(int i = 0; i < info[node].Count; i++)
+        {
+            if(info[node][i] != parent)
+            {
                 DFS(info[node][i], node, info, isLightOn, ref answer);
                 
-                if(!isLightOn[info[node][i]] && !isLightOn[node]){
+                if(!isLightOn[info[node][i]] && !isLightOn[node])
+                {
                     isLightOn[node] = true;
                     answer++;
                 }
