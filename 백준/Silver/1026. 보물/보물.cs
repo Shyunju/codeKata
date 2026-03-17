d42da2383662bc5d3ab2412b129b1ac6f4f4f26b
SHIN HYUNJU <71755508+Shyunju@users.noreply.github.com>
2026-03-17 17:40:43 +0900

    [Silver IV] Title: 보물, Time: 52 ms, Memory: 6016 KB -BaekjoonHub

--- 백준/Silver/1026. 보물/보물.cs
+++ 백준/Silver/1026. 보물/보물.cs
@@ -1,35 +1,31 @@
 using System;
-namespace 백준
-{
-    internal class Program
-    {
-        
-        static void Main(string[] args)
-        {            
-            int n = int.Parse(Console.ReadLine());
-            int[] a = new int[n];
-            int[] b = new int[n];
+using System.Collections.Generic;
 
-            string num = Console.ReadLine();
-            string[] input = num.Split(' ');
-            for(int i = 0; i < n; i++){
-                a[i] = int.Parse(input[i]);
-            }
-            Array.Sort(a);
-            
-            num = Console.ReadLine();
-            string[] input2 = num.Split(' ');
-            for(int j = 0; j < n; j++){
-                b[j] = int.Parse(input2[j]);
-            }
-            Array.Sort(b);
-            Array.Reverse(b);
-            
-            int s = 0;
-            for(int k = 0; k < n; k++){
-                s += a[k] * b[k];
-            }
-            Console.WriteLine(s);
+public class Progrma
+{
+    static void Main(string[] args)
+    {
+        Progrma p = new Progrma();
+        
+        int n = int.Parse(Console.ReadLine());
+        string[] input1 = Console.ReadLine().Split();
+        string[] input2 = Console.ReadLine().Split();
+        List<int> a = new List<int>();
+        List<int> b = new List<int>();
+        
+        for(int i = 0; i <n; i++)
+        {
+            a.Add(int.Parse(input1[i]));
+            b.Add(int.Parse(input2[i]));
         }
+        a.Sort();
+        b.Sort();
+        b.Reverse();
+        int s = 0;
+        for(int i =0;i <n;i++)
+        {
+            s += a[i] * b[i];
+        }
+        Console.Write(s);
     }
 }
