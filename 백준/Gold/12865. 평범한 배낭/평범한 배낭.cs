fff9a9eb27cdc9ce82120f2ea7a87b633a6c04ac
SHIN HYUNJU <71755508+Shyunju@users.noreply.github.com>
2026-03-23 17:23:08 +0900

    [Gold V] Title: 평범한 배낭, Time: 64 ms, Memory: 6496 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.cs
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.cs
@@ -6,25 +6,32 @@ public class Solution
     {
         Solution s = new Solution();
         var input = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
+        
         int n = input[0];
         int limit = input[1];
+        
         List<(int w, int v)> list = new List<(int w, int v)>();
+        
         for(int i =0;i < n; i++)
         {
             var ob = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
             list.Add((ob[0], ob[1]));
         }
+        
         int[] dp = new int[limit+1];
+        
         for(int i = 0; i < list.Count(); i++)
         {
             var cur = list[i];
             if(cur.w > limit)
                 continue;
+            
             for(int j = limit; j >= cur.w; j--)
             {
                 dp[j] = Math.Max(dp[j], dp[j - cur.w] + cur.v);
             }
         }
+        
         Console.Write(dp.Max());
     }
 }
