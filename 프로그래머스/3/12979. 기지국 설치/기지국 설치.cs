29d6c6db8eb2e0fa3b94e55a79e5e71752675588
SHIN HYUNJU <71755508+Shyunju@users.noreply.github.com>
2026-03-29 23:05:24 +0900

    [level 3] Title: 기지국 설치, Time: 0.31 ms, Memory: 31.2 MB -BaekjoonHub

--- 프로그래머스/3/12979. 기지국 설치/기지국 설치.cs
+++ 프로그래머스/3/12979. 기지국 설치/기지국 설치.cs
@@ -7,13 +7,17 @@ class Solution
         int width = 2 * w +1;
         int idx = 1;
         
-        foreach(int i in stations){
+        foreach(int i in stations)
+        {
             int blank = i - w - idx;
             if(blank > 0)
                 answer += (blank + width - 1) / width;
+            
             idx = i + w + 1;
         }
+        
         int last = n + 1 - idx;
+        
         if(last > 0)
             answer += (last + width -1) / width;
 
