bf5532b6ce4decd76c37717c41de123b195bae9c
SHIN HYUNJU <71755508+Shyunju@users.noreply.github.com>
2026-05-15 17:34:33 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 20.57 ms, Memory: 56.4 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.cs
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.cs
@@ -11,21 +11,28 @@ public class Solution {
         long sum1 = q1.Sum();
         long sum2 = q2.Sum();
         
-        if(sum1 == sum2) return 0;
+        if(sum1 == sum2) 
+            return 0;
         
         long sum = sum1 + sum2;
-        if(sum % 2 == 1) return -1;
+        if(sum % 2 == 1) 
+            return -1;
         
         long target = sum / (long)2;
         
-        while(sum1 != target){
-            if(answer >= maxCnt) return -1;
+        while(sum1 != target)
+        {
+            if(answer >= maxCnt) 
+                return -1;
             
-            if(sum1 < target){
+            if(sum1 < target)
+            {
                 long num = q2.Dequeue();
                 sum1 += num;
                 q1.Enqueue(num);
-            }else{
+            }
+            else
+            {
                 long num = q1.Dequeue();
                 sum1 -= num;
                 q2.Enqueue(num);
