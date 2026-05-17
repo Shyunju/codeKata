4eecded1c1ff5f4fca3698b5207033a475840c90
SHIN HYUNJU <71755508+Shyunju@users.noreply.github.com>
2026-05-17 23:24:46 +0900

    [level 2] Title: 예상 대진표, Time: 0.18 ms, Memory: 30.5 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.cs
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.cs
@@ -11,8 +11,16 @@ class Solution
         {
             if(B-A == 1 && B % 2 == 0)
                 break;
-            A = A % 2 == 0 ? A / 2 : A / 2 + 1;
-            B = B % 2 == 0 ? B / 2 : B / 2 + 1;
+            
+            if(A % 2 == 0)
+                A /= 2;
+            else
+                A = A / 2 + 1;
+            
+            if(B % 2 == 0)
+                B /= 2;
+            else
+                B = B / 2 + 1;
             answer++;
         }
 
