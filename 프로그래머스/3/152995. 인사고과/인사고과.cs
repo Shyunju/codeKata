57ad4884889bf0efed4f13b90792fae4b46b509b
SHIN HYUNJU <71755508+Shyunju@users.noreply.github.com>
2026-06-12 17:25:35 +0900

    [level 3] Title: 인사고과, Time: 29.47 ms, Memory: 42.8 MB -BaekjoonHub

--- 프로그래머스/3/152995. 인사고과/인사고과.cs
+++ 프로그래머스/3/152995. 인사고과/인사고과.cs
@@ -6,7 +6,8 @@ public class Solution {
         int myScore = scores[0,0] + scores[0,1];
         var scoreList = new List<(int, int)>();
         
-        for(int i = 0; i < scores.GetLength(0); i++){
+        for(int i = 0; i < scores.GetLength(0); i++)
+        {
             scoreList.Add((scores[i,0], scores[i,1]));
         }
         
@@ -16,9 +17,11 @@ public class Solution {
         });
         
         int max2Score = 0;
-        foreach((int, int)i in scoreList){
+        foreach((int, int)i in scoreList)
+        {
             if(scores[0, 0] < i.Item1 && scores[0,1] < i.Item2)  return -1;
-            if(i.Item2 >= max2Score){
+            if(i.Item2 >= max2Score)
+            {
                 if(myScore < i.Item1 + i.Item2) answer++;
                 max2Score = i.Item2;
             }
