d1e178035700cae01a5eecccbb22f0cc061737db
SHIN HYUNJU <71755508+Shyunju@users.noreply.github.com>
2026-09-16 17:20:20 +0900

    [level 3] Title: 부대복귀, Time: 175.62 ms, Memory: 138 MB -BaekjoonHub

--- 프로그래머스/3/132266. 부대복귀/부대복귀.cs
+++ 프로그래머스/3/132266. 부대복귀/부대복귀.cs
@@ -2,37 +2,65 @@ using System;
 using System.Collections.Generic;
 
 public class Solution {
+    public class Edge
+    {
+        public int To {get; set;}
+        public int Weight {get; set;}
+        
+        public Edge(int to, int weight)
+        {
+            To = to;
+            Weight = weight;
+        }
+    }
     public int[] solution(int n, int[,] roads, int[] sources, int destination) {
         int[] answer = new int[sources.Length];
-        var q = new Queue<(int, int)>();
-        var dict = new Dictionary<int, List<int>>();
-        bool[] visit = new bool[n+1];
-        
+        int[] distance = new int[n+1];
+        List<Edge>[] graph = new List<Edge>[n+1];
+        for(int i =0; i< n+1; i++)
+        {
+            graph[i] = new List<Edge>();
+        }
+        for(int i = 0; i < roads.GetLength(0); i++)
+        {
+            graph[roads[i,0]].Add(new Edge(roads[i,1], 1));
+            graph[roads[i,1]].Add(new Edge(roads[i,0], 1));
+        }
+        distance = Dijkstra(graph, destination, n);
         for(int i = 0; i < answer.Length; i++)
-            answer[i] = -1;
-        
-        for(int i = 0; i <= n; i++){
-            dict.Add(i, new List<int>());
-        }
-        for(int i = 0; i < roads.GetLength(0); i++){
-            dict[roads[i,0]].Add(roads[i,1]);
-            dict[roads[i,1]].Add(roads[i,0]);
+        {
+            
+            answer[i] = distance[sources[i]] == int.MaxValue ? -1: distance[sources[i]];
         }
         
-        q.Enqueue((destination, 0));
-        visit[destination] = true;
-        
-        while(q.Count > 0){
-            (int now, int count) = q.Dequeue();
-            for(int i= 0; i < dict[now].Count; i++){
-                if(visit[dict[now][i]]) continue;
-                visit[dict[now][i]] = true;
-                q.Enqueue((dict[now][i], count+1));
-            }
-            for(int i = 0; i < sources.Length; i++){
-                if(now == sources[i]) answer[i] = count;
-            }
-        }
         return answer;
+    }
+    public int[] Dijkstra(List<Edge>[] graph, int start, int n)
+    {
+        int[] dis = new int[n+1];
+        Array.Fill(dis, int.MaxValue);
+        
+        PriorityQueue<int, int> pq = new PriorityQueue<int, int>();
+        pq.Enqueue(start, 0);
+        dis[start] = 0;
+        
+        while(pq.Count > 0)
+        {
+            pq.TryDequeue(out int curNode, out int curDist);
+            if(dis[curNode] < curDist) continue;
+            
+            foreach(var edge in graph[curNode])
+            {
+                int nextNode = edge.To;
+                int nextDist = edge.Weight + curDist;
+                
+                if(dis[nextNode] > nextDist)
+                {
+                    dis[nextNode] = nextDist;
+                    pq.Enqueue(nextNode, nextDist);
+                }
+            }
+        }
+        return dis;
     }
 }
